b5be195cd839bb2563d7ce23549c03bebb5eb07a
Alejandro Medrano <amedrano@lst.tfo.upm.es>
2011-05-20 16:44:44 +0000

    More javaDoc
    moved MyOutputSubscriber form nested in GUIIOHandler to top level. This makes it easier to understand.

--- handler.gui/src/main/java/org/universAAL/ui/handler/gui/RepeatTableModel.java
+++ handler.gui/src/main/java/org/universAAL/ui/handler/gui/RepeatTableModel.java
@@ -16,7 +16,7 @@
 	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 	See the License for the specific language governing permissions and
 	limitations under the License.
-*/
+ */
 /**
  * 
  */
@@ -34,47 +34,50 @@ import org.universAAL.middleware.rdf.PropertyPath;
 import org.universAAL.middleware.owl.ManagedIndividual;
 
 /**
- * This class implements a multiple inheritance of {@link Repeat} and {@link AbstractTableModel}.
+ * This class implements a multiple inheritance of {@link Repeat} and
+ * {@link AbstractTableModel}.
  * 
  * @author mtazari
- *
+ * 
  */
 public class RepeatTableModel extends AbstractTableModel {
-	
-	public static final long serialVersionUID 
-	= RepeatTableModel.class.hashCode();
+
+	public static final long serialVersionUID = RepeatTableModel.class
+			.hashCode();
 
 	/**
 	 * {@link Repeat} object to be used.
 	 */
 	private Repeat repeat;
-	
+
 	/**
 	 * container for children of {@link #repeat}
 	 */
 	private FormControl[] elems;
-	
+
 	/**
 	 * Constructor method.
 	 * 
-	 * @param repeat initial {@link Repeat} object
+	 * @param repeat
+	 *            initial {@link Repeat} object
 	 */
 	public RepeatTableModel(Repeat repeat) {
 		this.repeat = repeat;
 		elems = repeat.getChildren();
-		if (elems == null  ||  elems.length != 1)
+		if (elems == null || elems.length != 1) {
 			throw new IllegalArgumentException("Malformed argument!");
+		}
 		if (elems[0] instanceof Group) {
 			elems = ((Group) elems[0]).getChildren();
-			if (elems == null  ||  elems.length == 0)
+			if (elems == null || elems.length == 0)
 				throw new IllegalArgumentException("Malformed argument!");
 		} else if (elems[0] == null)
 			throw new IllegalArgumentException("Malformed argument!");
 	}
-	
+
 	/**
-	 * Call {@link Repeat#addValue()} and 
-	 * then {@link AbstractTableModel#fireTableRowsInserted(int, int)}
+	 * Call {@link Repeat#addValue()} and then
+	 * {@link AbstractTableModel#fireTableRowsInserted(int, int)}
 	 */
 	void addValue() {
 		if (repeat.addValue()) {
@@ -82,10 +85,12 @@ public class RepeatTableModel extends AbstractTableModel {
 			fireTableRowsInserted(sel, sel);
 		}
 	}
-	
+
 	/**
 	 * Implements {@link TableModel#getColumnClass(int)}
-	 * @param col indicates column index 
+	 * 
+	 * @param col
+	 *            indicates column index
 	 */
 	public Class<?> getColumnClass(int col) {
 		String type = elems[col].getTypeURI();
@@ -100,7 +105,9 @@ public class RepeatTableModel extends AbstractTableModel {
 		return c;
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see javax.swing.table.TableModel#getColumnCount()
 	 */
 	/**
@@ -109,17 +116,19 @@ public class RepeatTableModel extends AbstractTableModel {
 	public int getColumnCount() {
 		return elems.length;
 	}
-	
+
 	/**
 	 * Implements {@link TableModel#getColumnName(int)}
-	 * @param col column Index
+	 * 
+	 * @param col
+	 *            column Index
 	 */
 	public String getColumnName(int col) {
 		Label l = elems[col].getLabel();
-		String answer = (l == null)? null : l.getText();
-		return (answer == null)? super.getColumnName(col) : answer;
+		String answer = (l == null) ? null : l.getText();
+		return (answer == null) ? super.getColumnName(col) : answer;
 	}
-	
+
 	/**
 	 * Getter for {@link #elems}
 	 */
@@ -127,7 +136,9 @@ public class RepeatTableModel extends AbstractTableModel {
 		return elems;
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see javax.swing.table.TableModel#getRowCount()
 	 */
 	/**
@@ -136,58 +147,66 @@ public class RepeatTableModel extends AbstractTableModel {
 	public int getRowCount() {
 		return repeat.getNumberOfValues();
 	}
-	
+
 	/**
 	 * 
-	 * @param col Column index.
+	 * @param col
+	 *            Column index.
 	 * @return the value contained in column.
 	 */
 	Object getSelectionColumnValue(int col) {
 		return elems[col].getValue();
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see javax.swing.table.TableModel#getValueAt(int, int)
 	 */
 	/**
 	 * Implements {@link TableModel#getValueAt(int, int)}
 	 * 
-	 * @param rowIndex index of the row
-	 * @param columnIndex index of the column
+	 * @param rowIndex
+	 *            index of the row
+	 * @param columnIndex
+	 *            index of the column
 	 * @return object conained at cell in position rowIndex, columnIndex
 	 */
 	public Object getValueAt(int rowIndex, int columnIndex) {
-		if (rowIndex < 0  ||  rowIndex >= repeat.getNumberOfValues()
-				||  columnIndex < 0  ||  columnIndex >= elems.length)
+		if (rowIndex < 0 || rowIndex >= repeat.getNumberOfValues()
+				|| columnIndex < 0 || columnIndex >= elems.length)
 			throw new ArrayIndexOutOfBoundsException();
 		repeat.setSelection(rowIndex);
-		PropertyPath path = (elems.length > 1)? elems[columnIndex].getReferencedPPath() : null;
-		String[] pp = (path == null)? null : path.getThePath();
+		PropertyPath path = (elems.length > 1) ? elems[columnIndex]
+				.getReferencedPPath() : null;
+		String[] pp = (path == null) ? null : path.getThePath();
 		return repeat.getValue(pp);
 	}
-	
+
 	/**
-	 * calls {@link Repeat#removeSelection()} for {@link #repeat} 
-	 * with the right arguments (calling {@link Repeat#getSelectionIndex()}
+	 * calls {@link Repeat#removeSelection()} for {@link #repeat} with the right
+	 * arguments (calling {@link Repeat#getSelectionIndex()}
 	 */
 	void removeValue() {
 		int sel = repeat.getSelectionIndex();
 		if (repeat.removeSelection())
 			fireTableRowsDeleted(sel, sel);
 	}
-	
+
 	/**
 	 * Call {@link Repeat#setSelection(int)} for {@link #repeat}
-	 * @param i index of the new selection
+	 * 
+	 * @param i
+	 *            index of the new selection
 	 */
 	void setSelection(int i) {
 		repeat.setSelection(i);
 	}
-	
+
 	/**
-	 * Call {@link Repeat#updateSelection()} for {@link #repeat}
-	 * with the right arguments (calling {@link Repeat#getSelectionIndex()}
-	 * then updates rows calling {@link AbstractTableModel#fireTableRowsUpdated(int, int)}
+	 * Call {@link Repeat#updateSelection()} for {@link #repeat} with the right
+	 * arguments (calling {@link Repeat#getSelectionIndex()} then updates rows
+	 * calling {@link AbstractTableModel#fireTableRowsUpdated(int, int)}
 	 */
 	void updateSelection() {
 		if (repeat.updateSelection()) {
@@ -196,19 +215,21 @@ public class RepeatTableModel extends AbstractTableModel {
 		}
 	}
 
-	// comment the below two methods in order to disallow the direct edit of the cells within the table
-	// so that always first a selection is set and then appropriate methods of repeat are called
-//	public boolean isCellEditable(int row, int col) {
-//		return elems[col] instanceof Input;
-//	}
-//	
-//	public void setValueAt(Object value, int rowIndex, int columnIndex) {
-//		if (columnIndex < 0  ||  columnIndex >= elems.length)
-//			throw new ArrayIndexOutOfBoundsException();
-//		repeat.setSelection(rowIndex);
-//		String[] pp = (elems.length > 1)?
-//				elems[columnIndex].getReferencedPPath().getThePath() : null;
-//		repeat.setValue(pp, value,
-//				elems[columnIndex].getControlRestrictions());
-//	}
+	// comment the below two methods in order to disallow the direct edit of the
+	// cells within the table
+	// so that always first a selection is set and then appropriate methods of
+	// repeat are called
+	// public boolean isCellEditable(int row, int col) {
+	// return elems[col] instanceof Input;
+	// }
+	//	
+	// public void setValueAt(Object value, int rowIndex, int columnIndex) {
+	// if (columnIndex < 0 || columnIndex >= elems.length)
+	// throw new ArrayIndexOutOfBoundsException();
+	// repeat.setSelection(rowIndex);
+	// String[] pp = (elems.length > 1)?
+	// elems[columnIndex].getReferencedPPath().getThePath() : null;
+	// repeat.setValue(pp, value,
+	// elems[columnIndex].getControlRestrictions());
+	// }
 }
