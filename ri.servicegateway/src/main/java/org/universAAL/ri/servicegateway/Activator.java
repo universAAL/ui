bab790e29b5abb78d07d852d0ef88386395a9ecd
mr Andrej Grguric <andrej.grguric@ericsson.com>
2011-09-13 14:13:20 +0000

    uAAL agreed code style applied

--- ri.servicegateway/src/main/java/org/universAAL/ri/servicegateway/Activator.java
+++ ri.servicegateway/src/main/java/org/universAAL/ri/servicegateway/Activator.java
@@ -16,51 +16,46 @@
 	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 	See the License for the specific language governing permissions and
 	limitations under the License.
-*/
+ */
 package org.universAAL.ri.servicegateway;
 
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 import org.osgi.util.tracker.ServiceTracker;
 
-
-
 /**
- * Main class for HTTP Services Gateway. It handles the registration
- * of servlets which provides different services based on URI.
- * It register a Login servlet and uses a Service Tracker to track for {@link GatewayPort} objects 
- * and register them using the <code>url</code>
- * and <code>dataDir</code> methods of GatewayPort.
- *  
+ * Main class for HTTP Services Gateway. It handles the registration of servlets
+ * which provides different services based on URI. It register a Login servlet
+ * and uses a Service Tracker to track for {@link GatewayPort} objects and
+ * register them using the <code>url</code> and <code>dataDir</code> methods of
+ * GatewayPort.
+ * 
  * @author Mario Latronico
- *
+ * 
  */
 
 public class Activator implements BundleActivator {
-	
-	
-	public static BundleContext context;
-	
-	GatewayPortTracker gatewayPortTracker;
-	ServiceTracker profileServiceTracker;
-	public void start(BundleContext context) {
 
-		Activator.context = context;
-		
-		// start the tracker on the GatewayPort name
-		gatewayPortTracker = new 
-		GatewayPortTracker(
-				context, 
-				GatewayPort.class.getName(),
-				null); // no service tracker customization
-		gatewayPortTracker.open();		
-		
-	}
-	
-	public void stop(BundleContext context) throws Exception {
+    public static BundleContext context;
 
-		gatewayPortTracker.close();
-	}
+    GatewayPortTracker gatewayPortTracker;
+    ServiceTracker profileServiceTracker;
+
+    public void start(BundleContext context) {
+
+	Activator.context = context;
+
+	// start the tracker on the GatewayPort name
+	gatewayPortTracker = new GatewayPortTracker(context, GatewayPort.class
+		.getName(), null); // no service tracker customization
+	gatewayPortTracker.open();
+
+    }
+
+    public void stop(BundleContext context) throws Exception {
+
+	gatewayPortTracker.close();
+    }
 
 } // end class
 
--- ri.servicegateway/src/main/java/org/universAAL/ri/servicegateway/GatewayPortTracker.java
+++ ri.servicegateway/src/main/java/org/universAAL/ri/servicegateway/GatewayPortTracker.java
@@ -16,7 +16,7 @@
 	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 	See the License for the specific language governing permissions and
 	limitations under the License.
-*/
+ */
 package org.universAAL.ri.servicegateway;
 
 import java.io.IOException;
@@ -43,172 +43,170 @@ import org.slf4j.LoggerFactory;
 
 public class GatewayPortTracker extends ServiceTracker implements Runnable {
 
-	private static int SLEEP_TIME = 3000;
-	private static final Logger log = LoggerFactory
-			.getLogger(GatewayPortTracker.class);
-	private HttpService httpService;
-	private BundleContext bc;
-	private Thread thread;
-	private Vector<ServiceReference> servletToBeRegistered;
-	private ServiceTracker httpTracker;
-	private Set<String> urlSet, dataDirSet;
+    private static int SLEEP_TIME = 3000;
+    private static final Logger log = LoggerFactory
+	    .getLogger(GatewayPortTracker.class);
+    private HttpService httpService;
+    private BundleContext bc;
+    private Thread thread;
+    private Vector<ServiceReference> servletToBeRegistered;
+    private ServiceTracker httpTracker;
+    private Set<String> urlSet, dataDirSet;
 
-	private class GatewayHttpContext implements HttpContext {
+    private class GatewayHttpContext implements HttpContext {
 
-		private ServiceReference reference;
+	private ServiceReference reference;
 
-		public GatewayHttpContext(ServiceReference reference) {
-			this.reference = reference;
-		}
+	public GatewayHttpContext(ServiceReference reference) {
+	    this.reference = reference;
+	}
 
-		public String getMimeType(String name) {
-			if (name.endsWith(".jpg") || name.endsWith(".jpeg")) {
-				return "image/jpeg";
-			} else if (name.endsWith(".gif")) {
-				return "image/gif";
-			} else if (name.endsWith(".html") || name.endsWith(".htm")) {
-				return "text/html";
-			} else if (name.endsWith(".png")) {
-				return "image/png";
-			} else if (name.endsWith(".css")) {
-				return "text/css";
-			} else if (name.endsWith(".xml")) {
-				return "text/xml";
-			} else if (name.endsWith(".js")) {
-				return "application/javascript";
-			} else {
-				return null;
-			}
-
-		}
-
-		public URL getResource(String res) {
-			return reference.getBundle().getResource(res);
-		}
-
-		public boolean handleSecurity(HttpServletRequest arg0,
-				HttpServletResponse arg1) throws IOException {
-
-			return true;
-		}
+	public String getMimeType(String name) {
+	    if (name.endsWith(".jpg") || name.endsWith(".jpeg")) {
+		return "image/jpeg";
+	    } else if (name.endsWith(".gif")) {
+		return "image/gif";
+	    } else if (name.endsWith(".html") || name.endsWith(".htm")) {
+		return "text/html";
+	    } else if (name.endsWith(".png")) {
+		return "image/png";
+	    } else if (name.endsWith(".css")) {
+		return "text/css";
+	    } else if (name.endsWith(".xml")) {
+		return "text/xml";
+	    } else if (name.endsWith(".js")) {
+		return "application/javascript";
+	    } else {
+		return null;
+	    }
 
 	}
 
-	public GatewayPortTracker(BundleContext bc, String clazz,
-			ServiceTrackerCustomizer stc) {
-		super(bc, clazz, stc);
-
-		servletToBeRegistered = new Vector<ServiceReference>();
-		urlSet = new HashSet<String>();
-		dataDirSet = new HashSet<String>();
-		
-		this.bc = bc;
-		httpTracker = new ServiceTracker(bc, HttpService.class.getName(), null); // no
-		httpTracker.open();
-
-		thread = new Thread(this, "GatewayPortTracker");
-		thread.start();
+	public URL getResource(String res) {
+	    return reference.getBundle().getResource(res);
 	}
 
+	public boolean handleSecurity(HttpServletRequest arg0,
+		HttpServletResponse arg1) throws IOException {
 
-	public synchronized void registerService(ServiceReference reference)
-			throws ServletException, NamespaceException {
-		GatewayPort gp = (GatewayPort) bc.getService(reference);
-		gp.setContext(reference.getBundle().getBundleContext());
-		HttpContext servletContext = new GatewayHttpContext(reference);
-		String url = gp.url();
-		String dataDir = gp.dataDir();
-		
-		
-		log.info("GATEWAY_TRACKER: Registering servlet, url -> " + url
-				+ " dataDir -> " + (dataDir == null ? "none" : dataDir));
-		// register the servlet to the url, for example /whereis, if it is
-		// already not registered
-		if (urlSet.add(url))
-			httpService.registerServlet(url, gp, null, servletContext);
-
-		// register also the path for the resources, for example
-		// /helpwhenoutside, if not registered
-		if (dataDir != null && dataDirSet.add(dataDir))
-			httpService.registerResources(dataDir, dataDir, servletContext);
-
+	    return true;
 	}
 
-	public synchronized Object addingService(ServiceReference reference) {
-		// if the http service is still not available, store the services
-		// to be registered when it will be available
-		if (httpService == null) {
-			servletToBeRegistered.add(reference);
-			return null;
+    }
+
+    public GatewayPortTracker(BundleContext bc, String clazz,
+	    ServiceTrackerCustomizer stc) {
+	super(bc, clazz, stc);
+
+	servletToBeRegistered = new Vector<ServiceReference>();
+	urlSet = new HashSet<String>();
+	dataDirSet = new HashSet<String>();
+
+	this.bc = bc;
+	httpTracker = new ServiceTracker(bc, HttpService.class.getName(), null); // no
+	httpTracker.open();
+
+	thread = new Thread(this, "GatewayPortTracker");
+	thread.start();
+    }
+
+    public synchronized void registerService(ServiceReference reference)
+	    throws ServletException, NamespaceException {
+	GatewayPort gp = (GatewayPort) bc.getService(reference);
+	gp.setContext(reference.getBundle().getBundleContext());
+	HttpContext servletContext = new GatewayHttpContext(reference);
+	String url = gp.url();
+	String dataDir = gp.dataDir();
+
+	log.info("GATEWAY_TRACKER: Registering servlet, url -> " + url
+		+ " dataDir -> " + (dataDir == null ? "none" : dataDir));
+	// register the servlet to the url, for example /whereis, if it is
+	// already not registered
+	if (urlSet.add(url))
+	    httpService.registerServlet(url, gp, null, servletContext);
+
+	// register also the path for the resources, for example
+	// /helpwhenoutside, if not registered
+	if (dataDir != null && dataDirSet.add(dataDir))
+	    httpService.registerResources(dataDir, dataDir, servletContext);
+
+    }
+
+    public synchronized Object addingService(ServiceReference reference) {
+	// if the http service is still not available, store the services
+	// to be registered when it will be available
+	if (httpService == null) {
+	    servletToBeRegistered.add(reference);
+	    return null;
+	}
+
+	GatewayPort gp = null;
+	try {
+	    // if there is some previously stored service first register it
+	    if (!servletToBeRegistered.isEmpty()) {
+		registerPreviousServices();
+	    }
+
+	    registerService(reference);
+
+	} catch (NamespaceException e) {
+	    // TODO Auto-generated catch block
+	    e.printStackTrace();
+	} catch (ServletException e) {
+	    // TODO Auto-generated catch block
+	    e.printStackTrace();
+	}
+	return gp;
+    }
+
+    public synchronized void removedService(ServiceReference reference,
+	    Object service) {
+
+	System.out.println("Removed: " + reference.toString());
+	GatewayPort gp = (GatewayPort) service;
+	httpService.unregister(gp.url());
+
+	super.removedService(reference, service);
+
+    }
+
+    public void run() {
+	Thread currentThread = Thread.currentThread();
+	while (thread == currentThread) {
+	    try {
+		HttpService httpService = (HttpService) httpTracker
+			.getService();
+		if (httpService != null) {
+		    this.httpService = httpService;
+		    registerPreviousServices();
+		    thread = null;
+		} else {
+		    Thread.sleep(SLEEP_TIME);
 		}
-
-		GatewayPort gp = null;
-		try {
-			// if there is some previously stored service first register it
-			if (!servletToBeRegistered.isEmpty()) {
-				registerPreviousServices();
-			}
-
-			registerService(reference);
-
-		} catch (NamespaceException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (ServletException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return gp;
+	    } catch (InterruptedException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	    } catch (ServletException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	    } catch (NamespaceException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	    }
 	}
 
-	public synchronized void removedService(ServiceReference reference,
-			Object service) {
-		
-		System.out.println("Removed: " + reference.toString());
-		GatewayPort gp = (GatewayPort) service;
-		httpService.unregister(gp.url());
-	
-		super.removedService(reference, service);
+    }
 
+    private synchronized void registerPreviousServices()
+	    throws ServletException, NamespaceException {
+
+	Iterator<ServiceReference> i = servletToBeRegistered.iterator();
+	while (i.hasNext()) {
+
+	    registerService(i.next());
 	}
+	servletToBeRegistered.clear();
 
-	public void run() {
-		Thread currentThread = Thread.currentThread();
-		while (thread == currentThread) {
-			try {
-				HttpService httpService = (HttpService) httpTracker
-						.getService();
-				if (httpService != null) {
-					this.httpService = httpService;
-					registerPreviousServices();
-					thread = null;
-				} else {
-					Thread.sleep(SLEEP_TIME);
-				}
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (ServletException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (NamespaceException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-
-	}
-
-	private synchronized void registerPreviousServices()
-			throws ServletException, NamespaceException {
-
-		Iterator<ServiceReference> i = servletToBeRegistered.iterator();
-		while (i.hasNext()) {
-
-			registerService( i.next());
-		}
-		servletToBeRegistered.clear();
-
-	}
+    }
 
 }
