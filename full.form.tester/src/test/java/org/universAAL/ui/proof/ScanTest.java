f51c0242fce86055dbc7411abc74828bd49975ca
Alejandro Medrano <amedrano@lst.tfo.upm.es>
2011-12-20 14:52:45 +0000

    Test Correction

--- full.form.tester/src/test/java/org/universAAL/ui/proof/ScanTest.java
+++ full.form.tester/src/test/java/org/universAAL/ui/proof/ScanTest.java
@@ -23,7 +23,7 @@ public class ScanTest {
 
 	public static void main(String[] args) {
 		try {
-			String[] a = ISubscriber.getClassNamesFromPackage("org.universAAL.ui.full.test.forms");
+			String[] a = ISubscriber.getClassNamesFromPackage();
 			for (int i=0; i< a.length; i++)
 				System.out.println(a[i]);
 		} catch (IOException e) {
