4001491fd9e3d3f1d7f912dd2635f86fc789d262
Carsten Stocklöw <carsten.stockloew@igd.fraunhofer.de>
2012-01-25 14:10:22 +0000

    formatted Formatted as Java conventions (ref.D2.3A)

--- dm/src/main/java/org/universAAL/ui/dm/InputSubscriber.java
+++ dm/src/main/java/org/universAAL/ui/dm/InputSubscriber.java
@@ -19,160 +19,162 @@
  */
 package org.universAAL.ui.dm;
 
-import org.universAAL.middleware.rdf.Resource;
 import org.universAAL.middleware.container.ModuleContext;
 import org.universAAL.middleware.container.utils.LogUtils;
 import org.universAAL.middleware.input.InputEvent;
+import org.universAAL.middleware.rdf.Resource;
 import org.universAAL.middleware.service.ServiceRequest;
 
 /**
  * The InputSubscriber subscribes to and handles all events from the input bus
  * that are interesting for the Dialog Manager.
- *  
+ * 
  * @author mtazari
  * 
  */
 public class InputSubscriber extends
-		org.universAAL.middleware.input.InputSubscriber {
-	
-	InputSubscriber(ModuleContext context) {
-		super(context);
-		// register for all context-free user input
-		addNewRegParams(null);
-	}
+	org.universAAL.middleware.input.InputSubscriber {
 
-	@Override
-	public void dialogAborted(String dialogID) {
-		Activator.getOutputPublisher().processAbortConfirmation(dialogID);
-	}
+    InputSubscriber(ModuleContext context) {
+	super(context);
+	// register for all context-free user input
+	addNewRegParams(null);
+    }
 
-	/**
-	 * @see org.universAAL.middleware.input.InputSubscriber#communicationChannelBroken()
-	 */
-	@Override
-	public void communicationChannelBroken() {
-		// TODO Auto-generated method stub
-	}
+    @Override
+    public void dialogAborted(String dialogID) {
+	Activator.getOutputPublisher().processAbortConfirmation(dialogID);
+    }
 
-	/**
-	 * @see org.universAAL.middleware.input.InputSubscriber#handleInputEvent(org.universAAL.middleware.input.InputEvent)
-	 */
-	@Override
-	public void handleInputEvent(InputEvent event) {
-		Resource u = event.getUser();
-		if (InputEvent.uAAL_MAIN_MENU_REQUEST.equals(event.getDialogID())) {
-			OutputPublisher op = Activator.getOutputPublisher();
-			while (op == null) {
-				try {
-					Thread.sleep(1000);
-				} catch (Exception e) {
-				}
-				op = Activator.getOutputPublisher();
-			}
-			op.showMenu(u);
-		} else if (event.isServiceSearch())
-			Activator.getOutputPublisher().showSearchResults(u,
-					event.getInputSentence());
-		else if (event.hasDialogInput()) {
-			String submissionID = event.getSubmissionID();
-			if (submissionID == null) {
-				LogUtils.logWarn(Activator.getBundleContext(), InputSubscriber.class,
-						"handleInputEvent",
-						new Object[] { "sumission ID null!" }, null);
-				return;
-			}
-			if (OutputPublisher.ABORT_ALL_OPEN_DIALOGS_CALL
-					.equals(submissionID)) {
-				Activator.getOutputPublisher().abortAllOpenDialogs(u,
-						event.getSubmittedData());
-				Activator.getOutputPublisher().showMenu(u);
-			} else if (OutputPublisher.CLOSE_MESSAGES_CALL.equals(submissionID)) {
-				Activator.getOutputPublisher().closeMessages(u,
-						event.getSubmittedData());
-				Activator.getOutputPublisher().showMenu(u);
-			} else if (OutputPublisher.CLOSE_OPEN_DIALOGS_CALL
-					.equals(submissionID)) {
-				Activator.getOutputPublisher().closeOpenDialogs(u,
-						event.getSubmittedData());
-				Activator.getOutputPublisher().showMenu(u);
-			} else if (OutputPublisher.DELETE_ALL_MESSAGES_CALL
-					.equals(submissionID)) {
-				Activator.getOutputPublisher().deleteAllMessages(u,
-						event.getSubmittedData());
-				Activator.getOutputPublisher().showMenu(u);
-			} else if (OutputPublisher.EXIT_CALL.equals(submissionID)) {
-				// do nothing
-			} else if (OutputPublisher.MENU_CALL.equals(submissionID))
-				Activator.getOutputPublisher().showMenu(u);
-			else if (OutputPublisher.MESSAGES_CALL.equals(submissionID))
-				Activator.getOutputPublisher().showMessages(u);
-			else if (OutputPublisher.OPEN_DIALOGS_CALL.equals(submissionID))
-				Activator.getOutputPublisher().showOpenDialogs(u);
-			else if (OutputPublisher.SEARCH_CALL.equals(submissionID)) {
-				Object searchStr = event
-						.getUserInput(new String[] { InputEvent.PROP_INPUT_SENTENCE });
-				if (searchStr instanceof String)
-					Activator.getOutputPublisher().showSearchResults(u,
-							(String) searchStr);
-				else {
-					LogUtils.logInfo(Activator.getBundleContext(), InputSubscriber.class,
-							"handleInputEvent",
-							new Object[] { "Submission without effect: ",
-									submissionID }, null);
-					Activator.getOutputPublisher().showMenu(u);
-				}
-			} else if (submissionID
-					.startsWith(OutputPublisher.SWITCH_TO_CALL_PREFIX)) {
-				int idx = -1;
-				try {
-					idx = Integer.parseInt(submissionID
-							.substring(OutputPublisher.SWITCH_TO_CALL_PREFIX
-									.length()));
-				} catch (Exception e) {
-					idx = -1;
-				}
-				Activator.getOutputPublisher().switchTo(u,
-						event.getSubmittedData(), idx);
-			} else if (!Activator.getOutputPublisher().checkMessageFinish(
-					event.getDialogID(), submissionID)) {
-				// probably a menu event
-				ServiceRequest sr = MainMenu.getMenuInstance(u)
-						.getAssociatedServiceRequest(submissionID, u);
-				if (sr == null) {
-					LogUtils.logInfo(Activator.getBundleContext(), InputSubscriber.class,
-							"handleInputEvent",
-							new Object[] { "Submission without effect: ",
-									submissionID }, null);
-					MainMenu.setSelection(u, submissionID);
-					Activator.getOutputPublisher().showMenu(u);
-				} else {
-					LogUtils.logInfo(Activator.getBundleContext(), InputSubscriber.class,
-							"handleInputEvent",
-							new Object[] { "Trying to call: ",
-									event.getSubmissionID() }, null);
-					Activator.getServiceCaller().call(sr);
-				}
-			}
+    /**
+     * @see org.universAAL.middleware.input.InputSubscriber#communicationChannelBroken()
+     */
+    @Override
+    public void communicationChannelBroken() {
+	// TODO Auto-generated method stub
+    }
+
+    /**
+     * @see org.universAAL.middleware.input.InputSubscriber#handleInputEvent(org.universAAL.middleware.input.InputEvent)
+     */
+    @Override
+    public void handleInputEvent(InputEvent event) {
+	Resource u = event.getUser();
+	if (InputEvent.uAAL_MAIN_MENU_REQUEST.equals(event.getDialogID())) {
+	    OutputPublisher op = Activator.getOutputPublisher();
+	    while (op == null) {
+		try {
+		    Thread.sleep(1000);
+		} catch (Exception e) {
 		}
+		op = Activator.getOutputPublisher();
+	    }
+	    op.showMenu(u);
+	} else if (event.isServiceSearch())
+	    Activator.getOutputPublisher().showSearchResults(u,
+		    event.getInputSentence());
+	else if (event.hasDialogInput()) {
+	    String submissionID = event.getSubmissionID();
+	    if (submissionID == null) {
+		LogUtils.logWarn(Activator.getBundleContext(),
+			InputSubscriber.class, "handleInputEvent",
+			new Object[] { "sumission ID null!" }, null);
+		return;
+	    }
+	    if (OutputPublisher.ABORT_ALL_OPEN_DIALOGS_CALL
+		    .equals(submissionID)) {
+		Activator.getOutputPublisher().abortAllOpenDialogs(u,
+			event.getSubmittedData());
+		Activator.getOutputPublisher().showMenu(u);
+	    } else if (OutputPublisher.CLOSE_MESSAGES_CALL.equals(submissionID)) {
+		Activator.getOutputPublisher().closeMessages(u,
+			event.getSubmittedData());
+		Activator.getOutputPublisher().showMenu(u);
+	    } else if (OutputPublisher.CLOSE_OPEN_DIALOGS_CALL
+		    .equals(submissionID)) {
+		Activator.getOutputPublisher().closeOpenDialogs(u,
+			event.getSubmittedData());
+		Activator.getOutputPublisher().showMenu(u);
+	    } else if (OutputPublisher.DELETE_ALL_MESSAGES_CALL
+		    .equals(submissionID)) {
+		Activator.getOutputPublisher().deleteAllMessages(u,
+			event.getSubmittedData());
+		Activator.getOutputPublisher().showMenu(u);
+	    } else if (OutputPublisher.EXIT_CALL.equals(submissionID)) {
+		// do nothing
+	    } else if (OutputPublisher.MENU_CALL.equals(submissionID))
+		Activator.getOutputPublisher().showMenu(u);
+	    else if (OutputPublisher.MESSAGES_CALL.equals(submissionID))
+		Activator.getOutputPublisher().showMessages(u);
+	    else if (OutputPublisher.OPEN_DIALOGS_CALL.equals(submissionID))
+		Activator.getOutputPublisher().showOpenDialogs(u);
+	    else if (OutputPublisher.SEARCH_CALL.equals(submissionID)) {
+		Object searchStr = event
+			.getUserInput(new String[] { InputEvent.PROP_INPUT_SENTENCE });
+		if (searchStr instanceof String)
+		    Activator.getOutputPublisher().showSearchResults(u,
+			    (String) searchStr);
+		else {
+		    LogUtils.logInfo(Activator.getBundleContext(),
+			    InputSubscriber.class, "handleInputEvent",
+			    new Object[] { "Submission without effect: ",
+				    submissionID }, null);
+		    Activator.getOutputPublisher().showMenu(u);
+		}
+	    } else if (submissionID
+		    .startsWith(OutputPublisher.SWITCH_TO_CALL_PREFIX)) {
+		int idx = -1;
+		try {
+		    idx = Integer.parseInt(submissionID
+			    .substring(OutputPublisher.SWITCH_TO_CALL_PREFIX
+				    .length()));
+		} catch (Exception e) {
+		    idx = -1;
+		}
+		Activator.getOutputPublisher().switchTo(u,
+			event.getSubmittedData(), idx);
+	    } else if (!Activator.getOutputPublisher().checkMessageFinish(
+		    event.getDialogID(), submissionID)) {
+		// probably a menu event
+		ServiceRequest sr = MainMenu.getMenuInstance(u)
+			.getAssociatedServiceRequest(submissionID, u);
+		if (sr == null) {
+		    LogUtils.logInfo(Activator.getBundleContext(),
+			    InputSubscriber.class, "handleInputEvent",
+			    new Object[] { "Submission without effect: ",
+				    submissionID }, null);
+		    MainMenu.setSelection(u, submissionID);
+		    Activator.getOutputPublisher().showMenu(u);
+		} else {
+		    LogUtils.logInfo(Activator.getBundleContext(),
+			    InputSubscriber.class, "handleInputEvent",
+			    new Object[] { "Trying to call: ",
+				    event.getSubmissionID() }, null);
+		    Activator.getServiceCaller().call(sr);
+		}
+	    }
 	}
+    }
 
-	/**
-	 * Subscribes to all input events with a given dialog ID. When an input
-	 * event for this dialog occurs, the method
-	 * {@link #handleInputEvent(InputEvent)} is called.
-	 * 
-	 * @param dialogID ID of the dialog
-	 */
-	void subscribe(String dialogID) {
-		addNewRegParams(dialogID);
-	}
+    /**
+     * Subscribes to all input events with a given dialog ID. When an input
+     * event for this dialog occurs, the method
+     * {@link #handleInputEvent(InputEvent)} is called.
+     * 
+     * @param dialogID
+     *            ID of the dialog
+     */
+    void subscribe(String dialogID) {
+	addNewRegParams(dialogID);
+    }
 
-	/**
-	 * Unsubscribes from all input events with a given dialog ID.
-	 * 
-	 * @param dialogID ID of the dialog
-	 */
-	void unsubscribe(String dialogID) {
-		removeMatchingRegParams(dialogID);
-	}
+    /**
+     * Unsubscribes from all input events with a given dialog ID.
+     * 
+     * @param dialogID
+     *            ID of the dialog
+     */
+    void unsubscribe(String dialogID) {
+	removeMatchingRegParams(dialogID);
+    }
 }
