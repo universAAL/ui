dd3cf193498b10cb00dbbffae63335dafc38e2b0
Alejandro Medrano <amedrano@lst.tfo.upm.es>
2012-03-11 22:45:11 +0000

    Automatic tag of ui.dm.mobile version: 1.0.2-SNAPSHOT

---
+++ ui.dm.mobile-1.0.2-SNAPSHOT/src/main/java/org/universAAL/ui/dm/mobile/Activator.java
@@ -0,0 +1,208 @@
+/*
+	Copyright 2008-2010 Fraunhofer IGD, http://www.igd.fraunhofer.de
+	Fraunhofer-Gesellschaft - Institute of Computer Graphics Research 
+	
+	See the NOTICE file distributed with this work for additional 
+	information regarding copyright ownership
+	
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+	
+	  http://www.apache.org/licenses/LICENSE-2.0
+	
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+ */
+package org.universAAL.ui.dm.mobile;
+
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.universAAL.middleware.container.ModuleContext;
+import org.universAAL.middleware.container.osgi.uAALBundleContainer;
+import org.universAAL.middleware.container.osgi.util.Messages;
+import org.universAAL.middleware.container.utils.LogUtils;
+import org.universAAL.middleware.owl.supply.LevelRating;
+import org.universAAL.middleware.rdf.Resource;
+import org.universAAL.middleware.sodapop.msg.MessageContentSerializer;
+import org.universAAL.middleware.ui.UICaller;
+import org.universAAL.middleware.ui.owl.AccessImpairment;
+import org.universAAL.middleware.ui.owl.Gender;
+import org.universAAL.middleware.ui.owl.Modality;
+import org.universAAL.middleware.ui.owl.PrivacyLevel;
+import org.universAAL.ontology.HealthProfileOntology;
+import org.universAAL.ontology.ProfileOntology;
+import org.universAAL.ontology.profile.AssistedPersonProfile;
+import org.universAAL.ontology.profile.AssistedPerson;
+import org.universAAL.ontology.profile.health.HealthProfile;
+import org.universAAL.ontology.impairment.HearingImpairment;
+import org.universaal.ontology.profile.uipreferences.uipreferencesprofile.owl.InteractionPreferencesProfile;
+import org.universaal.ontology.profile.uipreferences.uipreferencesprofile.owl.UIPreferencesProfileOntology;
+import org.universaal.ontology.useridprofileontology.owl.UserIDProfile;
+import org.universaal.ontology.useridprofileontology.owl.UserIDProfileOntology;
+
+/**
+ * The bundle activator.
+ * 
+ * @author mtazari
+ */
+public class Activator extends Thread implements BundleActivator {
+
+    private static ModuleContext context = null;
+    static final Logger logger = LoggerFactory.getLogger(Activator.class);
+    private static MessageContentSerializer serializer = null;
+
+    private static UICaller outputPublisher = null;
+    private static ServiceCaller serviceCaller = null;
+
+    /**
+     * The configuration file with translated strings to show to the user.
+     */
+    private static Messages messages;
+
+    
+    /**
+     * Get the bundle context
+     * 
+     * @return The bundle context.
+     */
+    static ModuleContext getModuleContext() {
+	return context;
+    }
+
+
+
+    /**
+     * Get the message serializer which can be used to (de-)serialize RDF
+     * messages.
+     * 
+     * @return The message serializer.
+     */
+    static MessageContentSerializer getSerializer() {
+	return serializer;
+    }
+
+    /**
+     * Get service caller.
+     * 
+     * @return The service caller.
+     */
+    static ServiceCaller getServiceCaller() {
+	return serviceCaller;
+    }
+
+    /**
+     * Create a new user for testing purposes with some default values. The new
+     * user is then uploaded to the database.
+     * 
+     * @param uri
+     *            The URI of the user.
+     * @param name
+     *            The name of the user.
+     */
+    static void loadTestData(String uri, String name) {
+UserIDProfile uip = new UserIDProfile(ProfileOntology.NAMESPACE + name
+		+ "idprofile");
+	uip.setUSERNAME(name);
+	AssistedPersonProfile ep = new AssistedPersonProfile(
+		ProfileOntology.NAMESPACE + name + "profile");
+	ep.setProperty(UserIDProfileOntology.PROP_ID_PROFILE, uip);
+	HealthProfile hp = new HealthProfile(ProfileOntology.NAMESPACE + name
+		+ "healthprofile");
+	hp.setDisability(new AccessImpairment[] { new HearingImpairment(
+		LevelRating.middle) });
+	ep.setProperty(HealthProfileOntology.PROP_HEALTH_PROFILE, hp);
+	InteractionPreferencesProfile ppp = new InteractionPreferencesProfile(
+		ProfileOntology.NAMESPACE + name + "profile");
+	ppp.setInsensibleMaxResolutionX(1024);
+	ppp.setInsensibleMaxResolutionX(768);
+	ppp.setInsensibleVolumeLevel(85);
+	ppp.setPersonalMinResolutionX(176);
+	ppp.setPersonalMinResolutionY(320);
+	ppp.setPersonalVolumeLevel(60);
+	ppp
+		.setPrivacyLevelsMappedToInsensible(new PrivacyLevel[] { PrivacyLevel.knownPeopleOnly });
+	ppp.setPrivacyLevelsMappedToPersonal(new PrivacyLevel[] {
+		PrivacyLevel.intimatesOnly, PrivacyLevel.homeMatesOnly });
+	ppp.setVoiceGender(Gender.female);
+	ppp.setInteractionModality(Modality.gui);
+	ep
+		.setProperty(
+			UIPreferencesProfileOntology.PROP_INTERACTION_PREF_PROFILE,
+			ppp);
+	AssistedPerson eu = new AssistedPerson(uri);
+	eu.setProfile(ep);
+    }
+
+
+    /**
+     * Get a string from the configuration file.
+     * 
+     * @param key
+     *            Get the translated string for this key.
+     * @return The translated string.
+     */
+    static String getString(String key) {
+	return messages.getString(key);
+    }
+
+    /**
+     * Get the configuration file reader which contains the translated strings
+     * of all messages shown to the user. The configuration file reader is
+     * initialized with the correct path to the file system.
+     * 
+     * @return The configuration file reader.
+     */
+    static Messages getConfFileReader() {
+	return messages;
+    }
+
+    /**
+     * The bundle start method externalized in a separate thread for non-
+     * blocking initialization of this bundle.
+     */
+    public void run() {
+	try {
+	    messages = new Messages(context.getID());
+	} catch (Exception e) {
+	    LogUtils
+		    .logError(
+			    context,
+			    getClass(),
+			    "run",
+			    new Object[] { "Cannot initialize Dialog Manager externalized strings!" },
+			    e);
+	    return;
+	}
+
+	outputPublisher = new DialogManagerImpl(context);
+	serviceCaller = new ServiceCaller(context);
+    }
+
+    /**
+     * Method for OSGi bundle: start this bundle.
+     */
+    public void start(BundleContext context) throws Exception {
+	Activator.context = uAALBundleContainer.THE_CONTAINER
+		.registerModule(new Object[] { context });
+	ServiceReference sref = context
+		.getServiceReference(MessageContentSerializer.class.getName());
+	serializer = (sref == null) ? null : (MessageContentSerializer) context
+		.getService(sref);
+	start();
+    }
+
+    /**
+     * Method for OSGi bundle: stop this bundle.
+     */
+    public void stop(BundleContext arg0) throws Exception {
+	// TODO Auto-generated method stub
+    }
+}
