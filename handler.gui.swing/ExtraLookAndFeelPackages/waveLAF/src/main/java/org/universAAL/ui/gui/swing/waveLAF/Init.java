2dfb88515571a28981433bf0b35e15c5ac7d6de6
Alejandro Medrano <amedrano@lst.tfo.upm.es>
2012-05-24 13:37:05 +0000

    template for waveLAF

---
+++ handler.gui.swing/ExtraLookAndFeelPackages/waveLAF/src/main/java/org/universAAL/ui/gui/swing/waveLAF/Init.java
@@ -0,0 +1,25 @@
+package org.universAAL.ui.gui.swing.waveLAF;
+
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+import javax.swing.plaf.metal.MetalLookAndFeel;
+
+import org.universAAL.ui.handler.gui.swing.model.InitInterface;
+
+/**
+ * the initialization class.
+ * @author amedrano
+ */
+public class Init implements InitInterface {
+
+    /** {@inheritDoc} */
+    public void install() {
+        MetalLookAndFeel.setCurrentTheme(new ColorLAF());
+        try {
+            UIManager.setLookAndFeel(new MetalLookAndFeel());
+        } catch (UnsupportedLookAndFeelException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
