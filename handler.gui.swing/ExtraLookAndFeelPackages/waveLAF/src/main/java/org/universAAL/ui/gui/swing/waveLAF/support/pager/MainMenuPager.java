5b926042a639e8320bad9c5aeb17f0ea8e53cf16
Alejandro Medrano <amedrano@lst.tfo.upm.es>
2012-09-13 15:32:52 +0000

    added License

--- handler.gui.swing/ExtraLookAndFeelPackages/waveLAF/src/main/java/org/universAAL/ui/gui/swing/waveLAF/support/pager/MainMenuPager.java
+++ handler.gui.swing/ExtraLookAndFeelPackages/waveLAF/src/main/java/org/universAAL/ui/gui/swing/waveLAF/support/pager/MainMenuPager.java
@@ -1,8 +1,24 @@
+/*******************************************************************************
+ * Copyright 2012 Universidad Politécnica de Madrid
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
 package org.universAAL.ui.gui.swing.waveLAF.support.pager;
 
 import java.awt.BorderLayout;
 import java.awt.CardLayout;
 import java.awt.Component;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ContainerEvent;
@@ -17,6 +33,15 @@ import org.universAAL.ui.gui.swing.waveLAF.support.GradientLAF;
 import java.awt.event.MouseWheelListener;
 import java.awt.event.MouseWheelEvent;
 
+/**
+ * A Jcomponent that will display components in {@link Page}s. 
+ * These pages usually have low number of components, 
+ * arranged in {@link GridLayout}; the {@link MainMenuPager} will 
+ * create the necessary pages to fit all the components, and manage
+ * the user input to change between pages
+ * @author amedrano
+ *
+ */
 public class MainMenuPager extends GradientLAF {
 
 	/**
