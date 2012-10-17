240fa845a0b34621c7c3a0f7e5663af88a49137d
Alejandro Medrano <amedrano@lst.tfo.upm.es>
2012-10-17 13:23:47 +0000

    some improvements (still funny business with preferred height... which incapacitates the scroll to do it vertacally correct)

--- handler.gui.swing/src/test/java/swing/simple/tests/FormLayoutTest.java
+++ handler.gui.swing/src/test/java/swing/simple/tests/FormLayoutTest.java
@@ -7,6 +7,8 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import java.awt.FlowLayout;
+
+import javax.swing.JScrollPane;
 import javax.swing.JTextField;
 import javax.swing.JComboBox;
 import javax.swing.JButton;
@@ -48,7 +50,7 @@ public class FormLayoutTest extends JFrame {
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
+		setContentPane(new JScrollPane(contentPane));
 //		contentPane.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
 		contentPane.setLayout(new FormLayout());
 		
