74d937e3eb81f22fab726a778324d26b19a9d4f0
Alejandro Medrano <amedrano@lst.tfo.upm.es>
2012-10-22 14:38:35 +0000

    Refactored DM moving to trunk.

---
+++ dm/src/main/java/org/universAAL/ui/dm/userInteraction/mainMenu/FileMainMenuProvider.java
@@ -0,0 +1,110 @@
+/*******************************************************************************
+ * Copyright 2012 Universidad Politécnica de Madrid
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package org.universAAL.ui.dm.userInteraction.mainMenu;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+import org.universAAL.middleware.container.utils.LogUtils;
+import org.universAAL.middleware.owl.supply.AbsLocation;
+import org.universAAL.middleware.rdf.Resource;
+import org.universAAL.middleware.service.ServiceRequest;
+import org.universAAL.middleware.ui.UIResponse;
+import org.universAAL.middleware.ui.rdf.Form;
+import org.universAAL.middleware.ui.rdf.Group;
+import org.universAAL.ui.dm.DialogManagerImpl;
+import org.universAAL.ui.dm.UserDialogManager;
+import org.universAAL.ui.dm.interfaces.MainMenuProvider;
+
+/**
+ * @author amedrano
+ *
+ */
+public class FileMainMenuProvider implements MainMenuProvider {
+
+    
+    private MainMenu mainMenu;
+    
+    private Set<String> entries = new TreeSet<String>();
+    
+    private UserDialogManager userDM;
+    
+    public FileMainMenuProvider(UserDialogManager udm){
+	userDM = udm;
+    }
+    
+    /** {@inheritDoc}*/
+    public void handle(UIResponse response) {
+	String submissionID = response.getSubmissionID();
+	if (entries.contains(submissionID)){
+	    ServiceRequest sr = mainMenu
+		    .getAssociatedServiceRequest(submissionID, response.getUser());
+	    if (sr == null) {
+		LogUtils.logInfo(DialogManagerImpl.getModuleContext(), getClass(),
+			"handleUIResponse", new Object[] {
+		    "Submission without effect: ", submissionID },
+		    null);
+		mainMenu.setSelection(submissionID);
+		showHierarchyMenu(submissionID);
+	    } else {
+		LogUtils.logInfo(DialogManagerImpl.getModuleContext(), getClass(),
+			"handleUIResponse", new Object[] { "Trying to call: ",
+		    response.getSubmissionID() }, null);
+		DialogManagerImpl.getServiceCaller().call(sr);
+	    }
+	}
+    }
+
+    private void showHierarchyMenu(String path) {
+	entries.clear();
+	Form f = Form.newSystemMenu(path);
+	mainMenu.addMenuRepresentation(f.getIOControls());
+	for (MenuNode entry : mainMenu.entries()) {
+	    entries.add(entry.getPath());
+	}
+	showNewMenu(f);
+    }
+
+    /** {@inheritDoc}*/
+    public Set<String> listDeclaredSubmitIds() {
+	return entries;
+    }
+
+    /** {@inheritDoc}*/
+    public Group getMainMenu(Resource user, AbsLocation location,
+	    Form systemForm) {
+	entries.clear();
+	Group main = systemForm.getIOControls();
+	if (mainMenu == null) {
+	    mainMenu = new MainMenu(DialogManagerImpl.getModuleContext(),
+		    user,
+		    userDM.getUserLocale());
+	}
+	mainMenu.resetSelection();
+	mainMenu.addMenuRepresentation(main);
+	for (MenuNode entry : mainMenu.entries()) {
+	    entries.add(entry.getPath());
+	}
+	return main;
+    }
+   
+    
+    private void showNewMenu(Form mff) {
+	userDM.add(this);
+	userDM.pushDialog(mff);
+    }
+
+}
