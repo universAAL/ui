17e0246b054a3e894437c75aa1824fbb9026ace3
mr Andrej Grguric <andrej.grguric@ericsson.com>
2012-11-07 16:05:39 +0000

    licence renamed to ASL2.0, logger added to adaptorKrakow class

--- dm/src/main/java/org/universAAL/ui/dm/adapters/AdaptorKrakow.java
+++ dm/src/main/java/org/universAAL/ui/dm/adapters/AdaptorKrakow.java
@@ -1,7 +1,9 @@
 package org.universAAL.ui.dm.adapters;
 
+import org.universAAL.middleware.container.utils.LogUtils;
 import org.universAAL.middleware.ui.UIRequest;
 import org.universAAL.middleware.ui.owl.Modality;
+import org.universAAL.ui.dm.DialogManagerImpl;
 import org.universAAL.ui.dm.interfaces.Adapter;
 
 public class AdaptorKrakow implements Adapter {
@@ -10,19 +12,20 @@ public class AdaptorKrakow implements Adapter {
 
 	if (req.getAddressedUser().getURI().contains("saied")) {
 	    req.setPresentationModality(Modality.gui);
-	    System.err
-		    .println("dm pushDialog entered. forcing gui modality for: "
-			    + req.getAddressedUser().getURI
-
-			    ());
+	    LogUtils.logInfo(DialogManagerImpl.getModuleContext(), getClass(),
+		    "adapt",
+		    new String[] { "forcing gui modality for user saied" },
+		    null);
 	}
 	if (req.getAddressedUser().getURI().contains("jack")) {
 	    req.setPresentationModality(Modality.web);
-	    System.err
-		    .println("dm pushDialog entered. forcing web modality for: "
-			    + req.getAddressedUser().getURI
-
-			    ());
+	    LogUtils
+		    .logInfo(
+			    DialogManagerImpl.getModuleContext(),
+			    getClass(),
+			    "adapt",
+			    new String[] { "forcing web modality for user jack" },
+			    null);
 	}
 
     }
