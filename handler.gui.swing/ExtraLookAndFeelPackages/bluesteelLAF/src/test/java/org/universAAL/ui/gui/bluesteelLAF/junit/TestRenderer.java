12b8d190c087db679c8bbb5c86c8401ab55965ca
mr Andrej Grguric <andrej.grguric@ericsson.com>
2012-11-30 14:48:09 +0000

    rename to bluesteelLAF for test classes also

--- handler.gui.swing/ExtraLookAndFeelPackages/bluesteelLAF/src/test/java/org/universAAL/ui/gui/bluesteelLAF/junit/TestRenderer.java
+++ handler.gui.swing/ExtraLookAndFeelPackages/bluesteelLAF/src/test/java/org/universAAL/ui/gui/bluesteelLAF/junit/TestRenderer.java
@@ -13,42 +13,38 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  ******************************************************************************/
-package org.universAAL.ui.gui.waveLAF.junit;
+package org.universAAL.ui.gui.bluesteelLAF.junit;
 
 import java.util.Properties;
 
 import org.universAAL.ui.handler.gui.swing.*;
 
-
 public class TestRenderer extends Renderer {
-	
 
-	public static String SIMPLE_MANAGER =
-			"org.universAAL.ui.handler.gui.swing.formManagement.SimpleFormManager";
-	public static String HIERARCHICAL_MANAGER =
-			"org.universAAL.ui.handler.gui.swing.formManagement.HierarchicalFromManager";
-	public static String QUEUED_MANAGER =
-			"org.universAAL.ui.handler.gui.swing.formManagement.QueuedFormManager";
-	
-	public TestRenderer(String FromManager){
-		super();
-        loadProperties();
-        modelMapper = new ModelMapper(this);
-        loadFormManager(FromManager);
-        initLAF = modelMapper.initializeLAF();
-	}
-	
-	/**
-     * load configuration properties from a file, setting the
-     * default for those which are not defined.
+    public static String SIMPLE_MANAGER = "org.universAAL.ui.handler.gui.swing.formManagement.SimpleFormManager";
+    public static String HIERARCHICAL_MANAGER = "org.universAAL.ui.handler.gui.swing.formManagement.HierarchicalFromManager";
+    public static String QUEUED_MANAGER = "org.universAAL.ui.handler.gui.swing.formManagement.QueuedFormManager";
+
+    public TestRenderer(String FromManager) {
+	super();
+	loadProperties();
+	modelMapper = new ModelMapper(this);
+	loadFormManager(FromManager);
+	initLAF = modelMapper.initializeLAF();
+    }
+
+    /**
+     * load configuration properties from a file, setting the default for those
+     * which are not defined.
+     * 
      * @see Renderer#properties
      */
     protected void loadProperties() {
-        properties = new Properties();
-        properties.put(DEMO_MODE, "true");
-        properties.put("LookandFeel.package", "org.universAAL.ui.gui.swing.waveLAF");
-        properties.put(GUI_LOCATION, "Unkown");
+	properties = new Properties();
+	properties.put(DEMO_MODE, "true");
+	properties.put("LookandFeel.package",
+		"org.universAAL.ui.gui.swing.bluesteelLAF");
+	properties.put(GUI_LOCATION, "Unknown");
     }
 
-   
 }
