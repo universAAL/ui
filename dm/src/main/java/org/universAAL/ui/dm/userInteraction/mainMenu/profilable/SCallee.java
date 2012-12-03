f23d5475c086118e3acead2687871c47fe67574c
Carsten Stocklöw <carsten.stockloew@igd.fraunhofer.de>
2012-12-03 19:09:30 +0000

    add/remove menu entries via service bus

--- dm/src/main/java/org/universAAL/ui/dm/userInteraction/mainMenu/profilable/SCallee.java
+++ dm/src/main/java/org/universAAL/ui/dm/userInteraction/mainMenu/profilable/SCallee.java
@@ -17,12 +17,14 @@ import org.universAAL.ontology.profile.User;
 import org.universAAL.ontology.profile.service.ProfilingService;
 import org.universAAL.ontology.profile.ui.mainmenu.MenuEntry;
 import org.universAAL.ontology.profile.ui.mainmenu.MenuProfile;
+import org.universAAL.ui.dm.DialogManagerImpl;
 import org.universAAL.ui.dm.UserDialogManager;
 
 public class SCallee extends ServiceCallee {
 
     public static final String NAMESPACE = "http://ontology.igd.fhg.de/ui.dm.owl#";
     private static final String SRV_ADD_ENTRY = NAMESPACE + "addEntry";
+    private static final String SRV_REMOVE_ENTRY = NAMESPACE + "removeEntry";
     private static final String IN_USER_ID = NAMESPACE + "userID";
     private static final String IN_MENU_ENTRY = NAMESPACE + "menuEntry";
 
@@ -30,11 +32,9 @@ public class SCallee extends ServiceCallee {
     private static final ServiceResponse invalidInput = new ServiceResponse(
 	    CallStatus.serviceSpecificFailure);
 
-    private static ServiceProfile[] profiles = new ServiceProfile[1];
+    private static ServiceProfile[] profiles = new ServiceProfile[2];
 
     private ModuleContext context;
-	private File file;
-	private UserDialogManager userDM;
 
     static {
 	invalidInput.addOutput(new ProcessOutput(
@@ -44,22 +44,31 @@ public class SCallee extends ServiceCallee {
 	    void init() {
 		addFilteringInput(IN_USER_ID, User.MY_URI, 1, 1,
 			new String[] { ProfilingService.PROP_CONTROLS });
-		addInputWithAddEffect(IN_MENU_ENTRY, MenuEntry.MY_URI, 1,
-			1, new String[] { ProfilingService.PROP_CONTROLS,
+		addInputWithAddEffect(IN_MENU_ENTRY, MenuEntry.MY_URI, 1, 1,
+			new String[] { ProfilingService.PROP_CONTROLS,
 				Profilable.PROP_HAS_PROFILE,
 				Profile.PROP_HAS_SUB_PROFILE,
 				MenuProfile.PROP_ENTRY });
 		profiles[0] = myProfile;
 	    }
 	}.init();
-
+	new ProfilingService(SRV_REMOVE_ENTRY) {
+	    void init() {
+		addFilteringInput(IN_USER_ID, User.MY_URI, 1, 1,
+			new String[] { ProfilingService.PROP_CONTROLS });
+		addInputWithRemoveEffect(IN_MENU_ENTRY, MenuEntry.MY_URI, 1, 1,
+			new String[] { ProfilingService.PROP_CONTROLS,
+				Profilable.PROP_HAS_PROFILE,
+				Profile.PROP_HAS_SUB_PROFILE,
+				MenuProfile.PROP_ENTRY });
+		profiles[1] = myProfile;
+	    }
+	}.init();
     }
 
-    public SCallee(ModuleContext context, File mainMenuFile, UserDialogManager udm) {
+    public SCallee(ModuleContext context) {
 	super(context, profiles);
 	this.context = context;
-	this.file = mainMenuFile;
-	this.userDM = udm;
     }
 
     @Override
@@ -73,7 +82,7 @@ public class SCallee extends ServiceCallee {
 			context,
 			SCallee.class,
 			"handleCall",
-			new Object[] { " -- received request to add a new menu entry --" },
+			new Object[] { " -- received request to add a new/remove an existing menu entry --" },
 			null);
 	if (call == null)
 	    return null;
@@ -82,16 +91,18 @@ public class SCallee extends ServiceCallee {
 	if (operation == null)
 	    return null;
 
-	if (!operation.startsWith(SRV_ADD_ENTRY))
-	    return null;
-
 	Object userID = call.getInputValue(IN_USER_ID);
 	Object menuEntry = call.getInputValue(IN_MENU_ENTRY);
 	if (userID == null || menuEntry == null)
 	    return null;
 	((Resource) menuEntry).unliteral();
 
-	return addEntry((User) userID, (MenuEntry) menuEntry);
+	if (operation.startsWith(SRV_ADD_ENTRY)) {
+	    return addEntry((User) userID, (MenuEntry) menuEntry);
+	} else if (operation.startsWith(SRV_REMOVE_ENTRY)) {
+	    return removeEntry((User) userID, (MenuEntry) menuEntry);
+	}
+	return null;
     }
 
     private ServiceResponse addEntry(User user, MenuEntry entry) {
@@ -103,7 +114,7 @@ public class SCallee extends ServiceCallee {
 	    // load
 	    Resource r = null;
 	    try {
-		r = mainMenu.readMenu(file);
+		r = mainMenu.readMenu(getFile(user.getURI()));
 	    } catch (Exception e) {
 		// most likely, the menu file does not exist yet, so do nothing
 	    }
@@ -113,12 +124,11 @@ public class SCallee extends ServiceCallee {
 	    // add
 	    mp.addMenuEntry(entry);
 	    // save
-	    mainMenu.saveMenu(file, mp);
+	    mainMenu.saveMenu(getFile(user.getURI()), mp);
 	    LogUtils.logDebug(context, SCallee.class, "addEntry", new Object[] {
 		    "adding main menu entry: ",
 		    entry.getServiceClass().getURI() }, null);
-	    userDM.refreshMainMenu();
-
+	    refreshMainMenu(user.getURI());
 	} catch (Exception e) {
 	    LogUtils
 		    .logError(
@@ -130,5 +140,94 @@ public class SCallee extends ServiceCallee {
 	}
 
 	return new ServiceResponse(CallStatus.succeeded);
+    }
+
+    private ServiceResponse removeEntry(User user, MenuEntry entry) {
+	// add the menu entry: load the contents of the config file, add the
+	// entry, and then save the file again
+	try {
+	    RDFMainMenu mainMenu = new RDFMainMenu(context, null);
+
+	    // load
+	    Resource r = null;
+	    try {
+		r = mainMenu.readMenu(getFile(user.getURI()));
+	    } catch (Exception e) {
+		// most likely, the menu file does not exist yet, so do nothing
+	    }
+	    if (r == null) {
+		LogUtils
+			.logDebug(
+				context,
+				SCallee.class,
+				"removeEntry",
+				createLogMessage(
+					"trying to remove the main menu entry ",
+					user,
+					entry,
+					" failed because the main menu file does not exist "
+						+ "(returning succeeded because the menu entry does not exist afterwards)."),
+				null);
+		return new ServiceResponse(CallStatus.succeeded);
+	    }
+	    if (!(r instanceof MenuProfile)) {
+		LogUtils
+			.logDebug(
+				context,
+				SCallee.class,
+				"removeEntry",
+				createLogMessage(
+					"trying to remove the main menu entry ",
+					user, entry,
+					" failed because the main menu file has an invalid content"),
+				null);
+		return new ServiceResponse(CallStatus.serviceSpecificFailure);
+	    }
+	    MenuProfile mp = (MenuProfile) r;
+	    // remove
+	    mp.removeMenuEntry(entry);
+	    // save
+	    mainMenu.saveMenu(getFile(user.getURI()), mp);
+	    LogUtils.logDebug(context, SCallee.class, "removeEntry",
+		    createLogMessage("main menu entry removed: ", user, entry),
+		    null);
+	    refreshMainMenu(user.getURI());
+	} catch (Exception e) {
+	    LogUtils
+		    .logError(
+			    context,
+			    SCallee.class,
+			    "removeEntry",
+			    createLogMessage(
+				    "an error occured while trying to remove the menu entry: ",
+				    user, entry), e);
+	}
+
+	return new ServiceResponse(CallStatus.succeeded);
+    }
+
+    private File getFile(String userURI) {
+	return ProfilableFileMainMenuProvider.getMainMenuFile(userURI);
+    }
+
+    private void refreshMainMenu(String userURI) {
+	DialogManagerImpl dm = DialogManagerImpl.getInstance();
+	if (dm == null)
+	    return;
+	UserDialogManager udm = dm.getUDM(userURI);
+	if (udm == null)
+	    return;
+	udm.refreshMainMenu();
+    }
+
+    private Object[] createLogMessage(String msg, User user, MenuEntry entry) {
+	return new Object[] { msg, entry.getServiceClass(), " from vendor ",
+		entry.getVendor(), " for user ", user.getURI() };
+    }
+
+    private Object[] createLogMessage(String msg, User user, MenuEntry entry,
+	    String suffix) {
+	return new Object[] { msg, entry.getServiceClass(), " from vendor ",
+		entry.getVendor(), " for user ", user.getURI(), suffix };
     }
 }
