7d63d21ffbf5f40a954a78864759c70483610e39
Carsten Stocklöw <carsten.stockloew@igd.fraunhofer.de>
2012-12-07 21:12:17 +0000

    Automatic tag of ui.handler.gui.swing.classicLAF version: 0.1.2-SNAPSHOT

---
+++ ui.handler.gui.swing.classicLAF-0.1.2-SNAPSHOT/src/main/java/org/universAAL/ui/handler/gui/swing/classic/GroupLAF.java
@@ -0,0 +1,99 @@
+/*******************************************************************************
+ * Copyright 2011 Universidad Politécnica de Madrid
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package org.universAAL.ui.handler.gui.swing.classic;
+
+import java.awt.FlowLayout;
+
+import javax.swing.BorderFactory;
+import javax.swing.JComponent;
+import javax.swing.JPanel;
+
+import org.universAAL.middleware.ui.rdf.Group;
+import org.universAAL.middleware.ui.rdf.Label;
+import org.universAAL.middleware.ui.rdf.Submit;
+import org.universAAL.ui.handler.gui.swing.Renderer;
+import org.universAAL.ui.handler.gui.swing.classic.specialButtons.uCCButton;
+import org.universAAL.ui.handler.gui.swing.classic.specialButtons.uStoreButton;
+import org.universAAL.ui.handler.gui.swing.model.FormControl.GroupModel;
+
+/**
+ * @author pabril
+ * 
+ */
+public class GroupLAF extends GroupModel {
+
+    private static final String PROP_ODD = "http://ontology.itaca.es/ClassicGUI.owl#odd";
+    private boolean switchMe=false;
+    
+    /**
+     * Constructor.
+     * 
+     * @param control
+     *            the {@link Group} which to model
+     */
+    public GroupLAF(Group control, Renderer render) {
+	super(control, render);
+    }
+
+    @Override
+    public JComponent getNewComponent() {
+	needsLabel=false;
+	JPanel panel = new JPanel();
+	if (!this.isTheIOGroup() && !this.isTheMainGroup() && !this.isTheSubmitGroup()) {
+	    // Apply this only to non-official groups
+	    Group parent = fc.getParentGroup();
+	    if (parent != null) {
+		if (parent.getProperty(PROP_ODD) == null) {
+		    // I am odd, switch me (IOControls is Even [is 0])
+		    fc.setProperty(PROP_ODD, Boolean.TRUE);
+		    switchMe = true;
+		}
+	    }
+	    /* Logic:
+	     * Constant:	0 0 0 0 1 1 1 1     
+	     * Vertical:	0 0 1 1 0 0 1 1
+	     * Switch:		0 1 0 1 0 1 0 1
+	     * V Layout ->	0 1 1 0 0 0 1 1
+	     */
+	    if( (FormLAF.constant && FormLAF.vertical) || ( !FormLAF.constant && ( switchMe != FormLAF.vertical ) ) ){
+		panel.setLayout(new MyVerticalFlowLayout(FormLAF.vGroupValign, FormLAF.hgap, FormLAF.vgap));
+	    }else{
+		panel.setLayout(new FlowLayout(FormLAF.hGroupHalign, FormLAF.hgap, FormLAF.vgap));
+	    }
+	} else if (this.isTheMainGroup() && this.isInMainMenu()) {
+	    new Submit((Group) fc, new Label("uCC", null), uCCButton.SUBMIT_ID);
+	    new Submit((Group) fc, new Label("uStore", null), uStoreButton.SUBMIT_ID);
+	}
+	return panel;
+    }
+
+    /** {@inheritDoc} */
+    public void update() {
+	super.update();
+	if (!this.isTheIOGroup()) {
+	    if (fc.getLabel() != null) {
+		String title = fc.getLabel().getText();
+		if (title != null) {
+		    if (this.isTheMainGroup() || this.isTheSubmitGroup()) {
+			title = "";
+		    }
+		    jc.setBorder(BorderFactory.createTitledBorder(title));
+		}
+	    }
+	}
+    }
+
+}
---
+++ ui.handler.gui.swing.classicLAF-0.1.2-SNAPSHOT/src/main/java/org/universAAL/ui/handler/gui/swing/classic/SelectLAF.java
@@ -0,0 +1,193 @@
+/*******************************************************************************
+ * Copyright 2011 Universidad Politécnica de Madrid
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package org.universAAL.ui.handler.gui.swing.classic;
+
+import java.awt.BorderLayout;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.BorderFactory;
+import javax.swing.JComponent;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTree;
+import javax.swing.ListSelectionModel;
+import javax.swing.event.TreeModelListener;
+import javax.swing.tree.TreeModel;
+import javax.swing.tree.TreePath;
+
+import org.universAAL.middleware.ui.rdf.ChoiceItem;
+import org.universAAL.middleware.ui.rdf.ChoiceList;
+import org.universAAL.middleware.ui.rdf.Label;
+import org.universAAL.middleware.ui.rdf.Select;
+import org.universAAL.ui.handler.gui.swing.Renderer;
+import org.universAAL.ui.handler.gui.swing.model.FormControl.SelectModel;
+
+/**
+ * @author pabril
+ * 
+ */
+public class SelectLAF extends SelectModel {
+
+    /**
+     * {@link JScrollPane} around the {@link JList}
+     */
+    JScrollPane sp = null;
+
+    /**
+     * Enveloped {@link JComponent}
+     */
+    JComponent ejc;
+
+    /**
+     * Constructor.
+     * 
+     * @param control
+     *            the {@link Select} which to model.
+     */
+    public SelectLAF(Select control, Renderer render) {
+	super(control, render);
+    }
+
+    public JComponent getNewComponent() {
+	Select form = (Select) fc;
+	needsLabel = false;
+	JComponent center;
+	
+        Label[] items = ((Select) fc).getChoices();
+        if (!((Select) fc).isMultilevel()) {
+            JList list = new JList(items);
+            list.setVisibleRowCount(5);
+            list.addListSelectionListener(this);
+            list.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
+            Object val = ((Select) fc).getValue();
+		if (val instanceof List){
+			selected = new ArrayList((List) val);
+		}
+		else if (val instanceof Object[]){
+			selected = new ArrayList();
+			for (int i = 0; i < ((Object[]) val).length; i++){
+				selected.add(((Object[]) val)[i]);
+			}
+		}
+		else {
+			selected = new ArrayList();
+			selected.add(val);
+		}
+            for (int i = 0; i < items.length; i++) {
+            	if (selected.contains(items[i])) {
+            		list.setSelectedIndex(i);
+            	}
+            }
+            list.addListSelectionListener(this);
+            center = list;
+        } else {
+            JTree jt = new JTree(new SelectionTreeModel());
+            jt.setVisibleRowCount(5);
+            jt.setEditable(false);
+            center = jt;
+        }
+        center.setBorder(BorderFactory.createEtchedBorder(ColorLAF.WHITE_DARK,ColorLAF.WHITE_MEDIUM));
+        
+        JPanel combined=new JPanel(new BorderLayout(5,5));
+//	combined.add(new JLabel(" "), BorderLayout.EAST);
+//	combined.add(new JLabel(" "), BorderLayout.NORTH);
+//	combined.add(new JLabel(" "), BorderLayout.SOUTH);
+	combined.add(center, BorderLayout.CENTER);
+	if (form.getLabel()!=null){
+	    String title=form.getLabel().getText();
+	    if(title!=null && !title.isEmpty()){
+		combined.add(new JLabel(title), BorderLayout.WEST);
+	    }/*else{
+		combined.add(new JLabel(" "), BorderLayout.WEST);
+	    }*/
+	}
+	
+	return combined;
+    }
+    
+    
+
+    @Override
+    protected void update() {
+	// Do nothing to avoid super
+    }
+
+
+
+    //TODO Remove this by making it public in model
+    protected class SelectionTreeModel implements TreeModel {
+        Label[] root;
+
+        public Object getRoot() {
+            root = ((Select) fc).getChoices();
+            if (root.length == 1) {
+                return root[1];
+            }
+            else {
+                return root;
+            }
+        }
+
+        public Object getChild(Object parent, int index) {
+            if (parent == root) {
+                return root[index];
+            }
+            else {
+                return ((ChoiceList) parent).getChildren()[index];
+            }
+        }
+
+        public int getChildCount(Object parent) {
+            if (parent == root) {
+                return root.length;
+            }
+            else {
+                return ((ChoiceList) parent).getChildren().length;
+            }
+        }
+
+        public boolean isLeaf(Object node) {
+            if (node == root) {
+                return root.length == 0;
+            }
+            else {
+                return node instanceof ChoiceItem;
+            }
+        }
+
+        public void valueForPathChanged(TreePath path, Object newValue) {
+            // XXX editable trees?
+        }
+
+        public int getIndexOfChild(Object parent, Object child) {
+            Label[] children = ((ChoiceList) parent).getChildren();
+            int i = 0;
+            while (i < children.length && children[i] != child) 
+            { i++; }
+            return i;
+        }
+
+        public void addTreeModelListener(TreeModelListener l) {
+        }
+
+        public void removeTreeModelListener(TreeModelListener l) {
+        }
+
+    }
+}
