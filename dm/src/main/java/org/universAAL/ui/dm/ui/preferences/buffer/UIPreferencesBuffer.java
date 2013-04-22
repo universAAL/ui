6ca470054fa95132886c39e6d565f1150a5b3972
mr Andrej Grguric <andrej.grguric@ericsson.com>
2013-04-22 16:02:44 +0000

    1st phase of integrating ui.dm.preferences into dm

---
+++ dm/src/main/java/org/universAAL/ui/dm/ui/preferences/buffer/UIPreferencesBuffer.java
@@ -0,0 +1,355 @@
+/*******************************************************************************
+ * Copyright 2013 Ericsson Nikola Tesla d.d.
+ *
+ * Licensed under both Apache License, Version 2.0 and MIT License.
+ *
+ * See the NOTICE file distributed with this work for additional 
+ * information regarding copyright ownership
+ *	
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package org.universAAL.ui.dm.ui.preferences.buffer;
+
+import java.util.HashSet;
+import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import org.universAAL.middleware.container.ModuleContext;
+import org.universAAL.middleware.container.utils.LogUtils;
+import org.universAAL.middleware.ui.DialogManager;
+import org.universAAL.middleware.ui.UIRequest;
+import org.universAAL.middleware.ui.owl.Modality;
+import org.universAAL.ontology.profile.SubProfile;
+import org.universAAL.ontology.profile.User;
+import org.universAAL.ontology.profile.UserProfile;
+import org.universAAL.ontology.ui.preferences.AccessMode;
+import org.universAAL.ontology.ui.preferences.AlertPreferences;
+import org.universAAL.ontology.ui.preferences.AlertType;
+import org.universAAL.ontology.ui.preferences.AuditoryPreferences;
+import org.universAAL.ontology.ui.preferences.ColorType;
+import org.universAAL.ontology.ui.preferences.ContentDensityType;
+import org.universAAL.ontology.ui.preferences.GeneralInteractionPreferences;
+import org.universAAL.ontology.ui.preferences.GenericFontFamily;
+import org.universAAL.ontology.ui.preferences.Intensity;
+import org.universAAL.ontology.ui.preferences.Language;
+import org.universAAL.ontology.ui.preferences.MainMenuConfigurationType;
+import org.universAAL.ontology.ui.preferences.PendingDialogsBuilderType;
+import org.universAAL.ontology.ui.preferences.PendingMessageBuilderType;
+import org.universAAL.ontology.ui.preferences.Size;
+import org.universAAL.ontology.ui.preferences.Status;
+import org.universAAL.ontology.ui.preferences.SystemMenuPreferences;
+import org.universAAL.ontology.ui.preferences.UIPreferencesSubProfile;
+import org.universAAL.ontology.ui.preferences.VisualPreferences;
+import org.universAAL.ontology.ui.preferences.VoiceGender;
+import org.universAAL.ontology.ui.preferences.WindowLayoutType;
+import org.universAAL.ui.dm.ui.preferences.caller.helpers.UIPreferencesSubprofileHelper;
+
+/**
+ * Acts as a buffer to store UI Preferences for logged in users so that
+ * {@link DialogManager} does not have to retrieve it from the Profiling Server
+ * when each {@link UIRequest} arrives but instead it retrieves it in some
+ * configured time interval.
+ * 
+ * Currently it also initializes UI Preferences subprofile based on stereotype
+ * data for (Assisted Person or Remote) users.
+ * 
+ * @author eandgrg
+ * 
+ */
+public class UIPreferencesBuffer {
+    ModuleContext mcontext;
+    public UIPreferencesSubprofileHelper uiPreferencesSubprofileHelper = null;
+    private Map<User, UIPreferencesSubProfile> userCurrentUIPreferencesSubProfileMap = null;
+
+    /**
+     * Timer for contacting Profiling Server
+     * */
+    private Timer getUIPreferencesTimer;
+
+    private static final String CONTACT_PROF_SERVER_DEFAULT_WAIT = "60000";
+
+    private static final String CONTACT_PROF_SERVER_WAIT = "ui.dm.contactProfilingServer.wait";
+
+    /**
+     * List with the unique names of the logged in users for which the UI
+     * Preferences are to be retrieved from the Profiling Server
+     */
+    private Set<User> currentlyLoggedInUsers = null;
+
+    public UIPreferencesBuffer(ModuleContext mcontext) {
+	this.mcontext = mcontext;
+
+	// Hashtable is synchonized, Hashmap not. Hashtable object cannot accept
+	// null (for K or V). If it previously contained a mapping for the
+	// key, the old value is replaced by the specified value.
+	userCurrentUIPreferencesSubProfileMap = new Hashtable<User, UIPreferencesSubProfile>();
+
+	uiPreferencesSubprofileHelper = new UIPreferencesSubprofileHelper(
+		mcontext);
+
+	// TODO populate this set with the users!
+	currentlyLoggedInUsers = new HashSet<User>();
+
+	Long period = Long.parseLong(System.getProperty(
+		CONTACT_PROF_SERVER_WAIT, CONTACT_PROF_SERVER_DEFAULT_WAIT));
+
+	getUIPreferencesTimer = new Timer(true);
+	getUIPreferencesTimer.scheduleAtFixedRate(new GetUIPreferencesTask(),
+		period, period);
+    }
+
+    /**
+     * GetUIPreferencesTask timer
+     * 
+     */
+    private class GetUIPreferencesTask extends TimerTask {
+	Iterator<User> it = null;
+	User tempUser = null;
+	UIPreferencesSubProfile tempUISubPrefProfile = null;
+
+	/** {@inheritDoc} */
+	@Override
+	public void run() {
+	    it = currentlyLoggedInUsers.iterator();
+	    // obtain ui preferences for all logged in users
+	    while (it.hasNext()) {
+		tempUser = it.next();
+
+		// fire service call to Profiling Server to obtain
+		// uiPreferencesSubprofile for current user
+		tempUISubPrefProfile = uiPreferencesSubprofileHelper
+			.getUIPreferencesSubProfileForUser(tempUser);
+
+		// store this subprofile for a given user
+		if (tempUISubPrefProfile != null) {
+		    userCurrentUIPreferencesSubProfileMap.put(tempUser,
+			    tempUISubPrefProfile);
+		}
+
+	    }
+	}
+    }
+
+    public void initializeUIPreferences(User user, boolean isAssistedPerson) {
+	// create ui subprofile
+	UIPreferencesSubProfile uiSubprofile = new UIPreferencesSubProfile(user
+		.getURI()
+		+ "SubprofileUIPreferences");
+
+	// TODO determine how to get this data (security.authorizator still not
+	// implemented!)
+	// determine the type of the user (is it AP or Remote user) and
+	// initialize UI Preferences
+	UIPreferencesSubProfile filledUISubprofile = null;
+	if (isAssistedPerson) {
+	    // assisted person has primary modality GUI
+	    filledUISubprofile = populateUIPreferencesWithStereotypeDataForAssistedPerson(uiSubprofile);
+	} else {
+	    // remote user has primary modality WEB
+	    filledUISubprofile = populateUIPreferencesWithStereotypeDataForRemoteUser(uiSubprofile);
+	}
+
+	// connecting filled in ui subprofile to user
+	uiPreferencesSubprofileHelper.addSubprofileToUser(user,
+		filledUISubprofile);
+
+	// remember connection between user and uiPrefsSubprofile (in creation
+	// time)so it does not
+	// have to be requested from Profiling Server when updating it
+	userCurrentUIPreferencesSubProfileMap.put(user, filledUISubprofile);
+
+    }
+
+    public UIPreferencesSubProfile populateUIPreferencesWithStereotypeDataForAssistedPerson(
+	    UIPreferencesSubProfile uiPrefsSubProfile) {
+	LogUtils
+		.logDebug(
+			mcontext,
+			this.getClass(),
+			"populateUIPreferencesWithStereotypeDataForAssistedPerson",
+			new Object[] { "ui.preferences initialization started for uiPrefsSubProfile: "
+				+ uiPrefsSubProfile.getURI() }, null);
+
+	GeneralInteractionPreferences generalInteractionPreferences = new GeneralInteractionPreferences(
+		uiPrefsSubProfile.getURI() + "GeneralInteractionPreferences");
+	generalInteractionPreferences
+		.setContentDensity(ContentDensityType.detailed);
+	generalInteractionPreferences.setPreferredLanguage(Language.english);
+	generalInteractionPreferences.setSecondaryLanguage(Language.english);
+	generalInteractionPreferences.setPreferredModality(Modality.gui);
+	generalInteractionPreferences.setSecondaryModality(Modality.voice);
+	uiPrefsSubProfile
+		.setInteractionPreferences(generalInteractionPreferences);
+
+	AlertPreferences alertPref = new AlertPreferences(uiPrefsSubProfile
+		.getURI()
+		+ "uiAlertPreferences");
+	alertPref.setAlertOption(AlertType.visualAndAudio);
+	uiPrefsSubProfile.setAlertPreferences(alertPref);
+
+	AccessMode accessMode = new AccessMode(uiPrefsSubProfile.getURI()
+		+ "uiAccessMode");
+	accessMode.setAuditoryModeStatus(Status.on);
+	accessMode.setOlfactoryModeStatus(Status.off);
+	accessMode.setTactileModeStatus(Status.off);
+	accessMode.setVisualModeStatus(Status.on);
+	accessMode.setTextualModeStatus(Status.on);
+	uiPrefsSubProfile.setAccessMode(accessMode);
+
+	AuditoryPreferences auditoryPreferences = new AuditoryPreferences(
+		uiPrefsSubProfile.getURI() + "AuditoryPreferences");
+	auditoryPreferences.setKeySoundStatus(Status.off);
+	auditoryPreferences.setPitch(Intensity.medium);
+	auditoryPreferences.setSpeechRate(Intensity.medium);
+	auditoryPreferences.setVolume(Intensity.medium);
+	auditoryPreferences.setVoiceGender(VoiceGender.female);
+	auditoryPreferences.setSystemSounds(Status.on);
+	uiPrefsSubProfile.setAudioPreferences(auditoryPreferences);
+
+	SystemMenuPreferences systemMenuPreferences = new SystemMenuPreferences(
+		uiPrefsSubProfile.getURI() + "SystemMenuPreferences");
+	systemMenuPreferences
+		.setMainMenuConfiguration(MainMenuConfigurationType.taskBar);
+	systemMenuPreferences.setMessagePersistance(Status.on);
+	systemMenuPreferences
+		.setPendingDialogBuilder(PendingDialogsBuilderType.table);
+	systemMenuPreferences
+		.setPendingMessageBuilder(PendingMessageBuilderType.simpleTable);
+	systemMenuPreferences.setSearchFeatureIsFirst(Status.on);
+	uiPrefsSubProfile.setSystemMenuPreferences(systemMenuPreferences);
+
+	VisualPreferences visualPreferences = new VisualPreferences(
+		uiPrefsSubProfile.getURI() + "VisualPreferences");
+	visualPreferences.setBackgroundColor(ColorType.lightBlue);
+	visualPreferences.setBrightness(Intensity.medium);
+	visualPreferences.setContentContrast(Intensity.high);
+	visualPreferences.setCursorSize(Size.medium);
+	visualPreferences.setDayNightMode(Status.on);
+	visualPreferences.setFlashingResources(Status.on);
+	visualPreferences.setFontColor(ColorType.black);
+	visualPreferences.setFontFamily(GenericFontFamily.serif);
+	visualPreferences.setFontSize(Size.medium);
+	visualPreferences.setHighlightColor(ColorType.white);
+	visualPreferences.setScreenResolution(Intensity.medium);
+	visualPreferences.setScreenSaverUsage(Status.off);
+	visualPreferences.setWindowLayout(WindowLayoutType.overlap);
+	uiPrefsSubProfile.setVisualPreferences(visualPreferences);
+
+	return uiPrefsSubProfile;
+    }
+
+    public UIPreferencesSubProfile populateUIPreferencesWithStereotypeDataForRemoteUser(
+	    UIPreferencesSubProfile uiPrefsSubProfile) {
+	LogUtils
+		.logDebug(
+			mcontext,
+			this.getClass(),
+			"populateUIPreferencesWithStereotypeDataForAssistedPerson",
+			new Object[] { "ui.preferences initialization started for uiPrefsSubProfile: "
+				+ uiPrefsSubProfile.getURI() }, null);
+
+	GeneralInteractionPreferences generalInteractionPreferences = new GeneralInteractionPreferences(
+		uiPrefsSubProfile.getURI() + "GeneralInteractionPreferences");
+	generalInteractionPreferences
+		.setContentDensity(ContentDensityType.detailed);
+	generalInteractionPreferences.setPreferredLanguage(Language.english);
+	generalInteractionPreferences.setSecondaryLanguage(Language.english);
+	generalInteractionPreferences.setPreferredModality(Modality.web);
+	generalInteractionPreferences.setSecondaryModality(Modality.gui);
+	uiPrefsSubProfile
+		.setInteractionPreferences(generalInteractionPreferences);
+
+	AlertPreferences alertPref = new AlertPreferences(uiPrefsSubProfile
+		.getURI()
+		+ "uiAlertPreferences");
+	alertPref.setAlertOption(AlertType.visualOnly);
+	uiPrefsSubProfile.setAlertPreferences(alertPref);
+
+	AccessMode accessMode = new AccessMode(uiPrefsSubProfile.getURI()
+		+ "uiAccessMode");
+	accessMode.setAuditoryModeStatus(Status.on);
+	accessMode.setOlfactoryModeStatus(Status.off);
+	accessMode.setTactileModeStatus(Status.off);
+	accessMode.setVisualModeStatus(Status.on);
+	accessMode.setTextualModeStatus(Status.on);
+	uiPrefsSubProfile.setAccessMode(accessMode);
+
+	AuditoryPreferences auditoryPreferences = new AuditoryPreferences(
+		uiPrefsSubProfile.getURI() + "AuditoryPreferences");
+	auditoryPreferences.setKeySoundStatus(Status.off);
+	auditoryPreferences.setPitch(Intensity.medium);
+	auditoryPreferences.setSpeechRate(Intensity.medium);
+	auditoryPreferences.setVolume(Intensity.medium);
+	auditoryPreferences.setVoiceGender(VoiceGender.female);
+	auditoryPreferences.setSystemSounds(Status.on);
+	uiPrefsSubProfile.setAudioPreferences(auditoryPreferences);
+
+	SystemMenuPreferences systemMenuPreferences = new SystemMenuPreferences(
+		uiPrefsSubProfile.getURI() + "SystemMenuPreferences");
+	systemMenuPreferences
+		.setMainMenuConfiguration(MainMenuConfigurationType.taskBar);
+	systemMenuPreferences.setMessagePersistance(Status.on);
+	systemMenuPreferences
+		.setPendingDialogBuilder(PendingDialogsBuilderType.table);
+	systemMenuPreferences
+		.setPendingMessageBuilder(PendingMessageBuilderType.simpleTable);
+	systemMenuPreferences.setSearchFeatureIsFirst(Status.on);
+	uiPrefsSubProfile.setSystemMenuPreferences(systemMenuPreferences);
+
+	VisualPreferences visualPreferences = new VisualPreferences(
+		uiPrefsSubProfile.getURI() + "VisualPreferences");
+	visualPreferences.setBackgroundColor(ColorType.lightBlue);
+	visualPreferences.setBrightness(Intensity.medium);
+	visualPreferences.setContentContrast(Intensity.high);
+	visualPreferences.setCursorSize(Size.medium);
+	visualPreferences.setDayNightMode(Status.on);
+	visualPreferences.setFlashingResources(Status.on);
+	visualPreferences.setFontColor(ColorType.black);
+	visualPreferences.setFontFamily(GenericFontFamily.serif);
+	visualPreferences.setFontSize(Size.medium);
+	visualPreferences.setHighlightColor(ColorType.black);
+	visualPreferences.setScreenResolution(Intensity.medium);
+	visualPreferences.setScreenSaverUsage(Status.off);
+	visualPreferences.setWindowLayout(WindowLayoutType.overlap);
+	uiPrefsSubProfile.setVisualPreferences(visualPreferences);
+
+	return uiPrefsSubProfile;
+    }
+
+    /**
+     * @return the {@link UIPreferencesSubProfile}
+     */
+    public UIPreferencesSubProfile getUIPreferencesSubprofileForUser(User user) {
+	return userCurrentUIPreferencesSubProfileMap.get(user);
+    }
+
+    /**
+     * @param userToAdd
+     *            User that is to be added in a set
+     * @return status, if this set already contains the user, the call leaves
+     *         the set unchanged and returns false
+     */
+    public boolean addLoggedInUsers(User userToAdd) {
+	return this.currentlyLoggedInUsers.add(userToAdd);
+    }
+
+    /**
+     * @return the old {@link UIPreferencesSubProfile} that was associated with
+     *         the {@link User} or null if there was no mapping
+     */
+    public UIPreferencesSubProfile changeCurrentUIPreferencesSubProfileForUser(
+	    User key, UIPreferencesSubProfile uiPrefSubprof) {
+	return this.userCurrentUIPreferencesSubProfileMap.put(key,
+		uiPrefSubprof);
+
+    }
+
+}
---
+++ dm/src/main/java/org/universAAL/ui/dm/ui/preferences/caller/helpers/UIPreferencesSubprofileHelper.java
@@ -0,0 +1,183 @@
+/*******************************************************************************
+ * Copyright 2013 Ericsson Nikola Tesla d.d.
+ *
+ * Licensed under both Apache License, Version 2.0 and MIT License.
+ *
+ * See the NOTICE file distributed with this work for additional 
+ * information regarding copyright ownership
+ *	
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package org.universAAL.ui.dm.ui.preferences.caller.helpers;
+
+import java.util.Iterator;
+import java.util.List;
+
+import org.universAAL.middleware.container.ModuleContext;
+import org.universAAL.middleware.container.utils.LogUtils;
+import org.universAAL.middleware.service.CallStatus;
+import org.universAAL.middleware.service.DefaultServiceCaller;
+import org.universAAL.middleware.service.ServiceCaller;
+import org.universAAL.middleware.service.ServiceRequest;
+import org.universAAL.middleware.service.ServiceResponse;
+import org.universAAL.ontology.profile.Profilable;
+import org.universAAL.ontology.profile.Profile;
+import org.universAAL.ontology.profile.SubProfile;
+import org.universAAL.ontology.profile.User;
+import org.universAAL.ontology.profile.UserProfile;
+import org.universAAL.ontology.profile.service.ProfilingService;
+import org.universAAL.ontology.ui.preferences.UIPreferencesSubProfile;
+import org.universAAL.ui.dm.ui.preferences.editor.UIPreferencesProvidedService;
+
+/**
+ * Helper class for making service requests towards Profiling Server in order to
+ * manipulate (add, change, get) {@link UIPreferencesSubProfile} data.
+ * 
+ * @author eandgrg
+ * 
+ */
+public class UIPreferencesSubprofileHelper {
+
+    private static final String OUTPUT_GETSUBPROFILES = UIPreferencesProvidedService.NAMESPACE
+	    + "outputSubprofile";
+
+    /**
+     * {@link ModuleContext}
+     */
+    private static ModuleContext mcontext;
+    private static ServiceCaller caller;
+
+    public UIPreferencesSubprofileHelper(ModuleContext mcontext) {
+	UIPreferencesSubprofileHelper.mcontext = mcontext;
+	caller = new DefaultServiceCaller(mcontext);
+    }
+
+    /**
+     * Adds given {@link SubProfile} to a Profiling Server and connects it with
+     * given {@link User}.
+     * 
+     * @param profilable
+     *            {@link User}
+     * @param subProfile
+     *            {@link SubProfile}
+     * @return call status as a {@link String}
+     */
+    public String addSubprofileToUser(User profilable, SubProfile subProfile) {
+	ServiceRequest req = new ServiceRequest(new ProfilingService(), null);
+	req.addValueFilter(new String[] { ProfilingService.PROP_CONTROLS },
+		profilable);
+	req.addAddEffect(new String[] { ProfilingService.PROP_CONTROLS,
+		Profilable.PROP_HAS_PROFILE, Profile.PROP_HAS_SUB_PROFILE },
+		subProfile);
+	ServiceResponse resp = caller.call(req);
+
+	LogUtils.logDebug(mcontext, this.getClass(), "addSubprofileToUser",
+		new Object[] { "ui subprofile: " + subProfile.getURI()
+			+ " created and connection attempt to user: "
+			+ profilable.getURI() + " returned status: "
+			+ resp.getCallStatus().name() }, null);
+
+	return resp.getCallStatus().name();
+    }
+
+    /**
+     * Updates given {@link SubProfile}.
+     * 
+     * @param subProfile
+     *            {@link SubProfile}
+     * @return call status as a {@link String}
+     */
+    public String changeSubProfile(SubProfile subProfile) {
+	ServiceRequest req = new ServiceRequest(new ProfilingService(), null);
+	req.addChangeEffect(new String[] { ProfilingService.PROP_CONTROLS,
+		Profilable.PROP_HAS_PROFILE, Profile.PROP_HAS_SUB_PROFILE },
+		subProfile);
+	ServiceResponse resp = caller.call(req);
+
+	LogUtils.logDebug(mcontext, this.getClass(), "changeSubProfile",
+		new Object[] { "ui subprofile: " + subProfile.getURI()
+			+ " change reguest returned status: "
+			+ resp.getCallStatus().name() }, null);
+
+	return resp.getCallStatus().name();
+    }
+
+    /**
+     * Retrieves {@link UIPreferencesSubProfile} that belongs to a given
+     * {@link User} from a Profiling Server (makes a service call). Note
+     * {@link User} and {@link UserProfile} should exist and be connected with
+     * {@link UIPreferencesSubProfile} before obtainment
+     * 
+     * @param user
+     *            {@link User}
+     * 
+     * @return {@link UIPreferencesSubProfile} that belongs to a given
+     *         {@link User} or null in all other cases.
+     */
+    public UIPreferencesSubProfile getUIPreferencesSubProfileForUser(User user) {
+	ServiceRequest req = new ServiceRequest(new ProfilingService(), null);
+	req.addValueFilter(new String[] { ProfilingService.PROP_CONTROLS },
+		user);
+	// with this restriction no matching service found is returned!!
+	// req.addTypeFilter(new String[] { ProfilingService.PROP_CONTROLS,
+	// Profilable.PROP_HAS_PROFILE, Profile.PROP_HAS_SUB_PROFILE },
+	// UIPreferencesSubProfile.MY_URI);
+	req.addRequiredOutput(OUTPUT_GETSUBPROFILES, new String[] {
+		ProfilingService.PROP_CONTROLS, Profilable.PROP_HAS_PROFILE,
+		Profile.PROP_HAS_SUB_PROFILE });
+	ServiceResponse resp = caller.call(req);
+	if (resp.getCallStatus() == CallStatus.succeeded) {
+	    try {
+		List<?> subProfiles = resp.getOutput(OUTPUT_GETSUBPROFILES,
+			true);
+		if (subProfiles == null || subProfiles.size() == 0) {
+		    LogUtils
+			    .logInfo(
+				    mcontext,
+				    this.getClass(),
+				    "getUIPreferencesSubProfilesForUser",
+				    new Object[] { "there are no UIPreference sub profiles for user: "
+					    + user.getURI() }, null);
+		    // TODO create one if there is none (dm initializes default
+		    // ones based on stereotype data for each user so this
+		    // should not be necessary here)
+		    return null;
+		}
+
+		Iterator<?> iter = subProfiles.iterator();
+		while (iter.hasNext()) {
+		    SubProfile subProfile = (SubProfile) iter.next();
+		    if (subProfile.getClassURI().equals(
+			    UIPreferencesSubProfile.MY_URI)) {
+			LogUtils
+				.logInfo(
+					mcontext,
+					this.getClass(),
+					"getUIPreferencesSubProfilesForUser",
+					new Object[] { "Following UIPreferencesSubProfile obtained from Profiling server: "
+						+ subProfile.getURI() }, null);
+			// TODO what if there is more UIPreferencesSubProfiles,
+			// currently only 1st is returned then
+			return (UIPreferencesSubProfile) subProfile;
+		    }
+		}
+		// TODO same as above comment
+		return null;
+	    } catch (Exception e) {
+		LogUtils.logError(mcontext, this.getClass(), "getSubProfile",
+			new Object[] { "exception: " }, e);
+		return null;
+	    }
+	} else {
+	    LogUtils
+		    .logWarn(mcontext, this.getClass(), "getSubProfile",
+			    new Object[] { "returned: "
+				    + resp.getCallStatus().name() }, null);
+	    return null;
+	}
+    }
+}
