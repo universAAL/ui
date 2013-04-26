071b2c09b91e995fd743bf1d99c75762692b231c
Alejandro Medrano <amedrano@lst.tfo.upm.es>
2013-04-26 13:46:34 +0000

    Improved tests, added testing for DialogPoolFileStore.

--- dm/src/test/java/org/universAAL/ui/dm/tests/ProfMainMenuTest.java
+++ dm/src/test/java/org/universAAL/ui/dm/tests/ProfMainMenuTest.java
@@ -3,17 +3,51 @@ package org.universAAL.ui.dm.tests;
 import java.io.InputStream;
 import java.util.Iterator;
 
+import org.universAAL.middleware.owl.DataRepOntology;
+import org.universAAL.middleware.owl.OntologyManagement;
+import org.universAAL.middleware.serialization.MessageContentSerializer;
+import org.universAAL.middleware.serialization.MessageContentSerializerEx;
+import org.universAAL.middleware.serialization.turtle.TurtleSerializer;
+import org.universAAL.middleware.ui.owl.UIBusOntology;
+import org.universAAL.ontology.location.LocationOntology;
+import org.universAAL.ontology.phThing.PhThingOntology;
+import org.universAAL.ontology.profile.ProfileOntology;
+import org.universAAL.ontology.profile.ui.mainmenu.MenuProfileOntology;
+import org.universAAL.ontology.shape.ShapeOntology;
+import org.universAAL.ontology.space.SpaceOntology;
+import org.universAAL.ontology.vcard.VCardOntology;
+import org.universAAL.plainJava.POJOModuleContext;
 import org.universAAL.ui.dm.userInteraction.mainMenu.MainMenu;
 import org.universAAL.ui.dm.userInteraction.mainMenu.MenuNode;
 import org.universAAL.ui.dm.userInteraction.mainMenu.profilable.RDFMainMenu;
 
-/*
+
 public class ProfMainMenuTest extends MainMenuTest {
 
-	public void test1() {
+	public void setUp(){
+		mc = new POJOModuleContext();
+		mc.getContainer().shareObject(mc,
+				new TurtleSerializer(),
+				new Object[] { MessageContentSerializer.class.getName() });
+		mc.getContainer().shareObject(mc,
+				new TurtleSerializer(),
+				new Object[] { MessageContentSerializerEx.class.getName() });
+
+		OntologyManagement.getInstance().register(mc, new DataRepOntology());
+    	OntologyManagement.getInstance().register(mc, new UIBusOntology());
+        OntologyManagement.getInstance().register(mc, new LocationOntology());
+        OntologyManagement.getInstance().register(mc, new ShapeOntology());
+        OntologyManagement.getInstance().register(mc, new PhThingOntology());
+        OntologyManagement.getInstance().register(mc, new SpaceOntology());
+        OntologyManagement.getInstance().register(mc, new VCardOntology());
+    	OntologyManagement.getInstance().register(mc, new ProfileOntology());
+		OntologyManagement.getInstance().register(mc, new MenuProfileOntology());
+	}
+	
+	public void testRead() {
 		InputStream is = getClass().getResourceAsStream("/prof_main_menu.txt");
 		assertNotNull(is);
-		MainMenu mm = new RDFMainMenu(null, is);
+		MainMenu mm = new RDFMainMenu(mc, is);
 		int i = 0;
 		Iterator<MenuNode> it = mm.entries().iterator();
 		while (it.hasNext()) {
@@ -29,4 +63,3 @@ public class ProfMainMenuTest extends MainMenuTest {
 	
 	
 }
-*/
