2cd76e83df23a8de2f694483ae58d3b7bc66b9e0
Carsten Stocklöw <carsten.stockloew@igd.fraunhofer.de>
2013-06-21 16:18:33 +0000

    Automatic tag of ui.resource.server version: 1.3.3-SNAPSHOT

---
+++ ui.resource.server-1.3.3-SNAPSHOT/src/test/java/org/universAAL/ui/resource/server/test/ArtifactIntegrationTest.java
@@ -0,0 +1,18 @@
+package org.universAAL.ui.resource.server.test;
+
+import org.universAAL.itests.IntegrationTest;
+
+/**
+ * For integration tests.
+ * 
+ * @author eandgrg
+ * 
+ */
+public class ArtifactIntegrationTest extends IntegrationTest {
+
+
+    public void testComposite() {
+	logAllBundles();
+    }
+
+}
