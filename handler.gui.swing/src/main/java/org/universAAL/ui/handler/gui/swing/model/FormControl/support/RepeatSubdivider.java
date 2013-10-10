afecf571d296b7f4ff1b760b4b6d1e049fb3a033
Alejandro Medrano <amedrano@lst.tfo.upm.es>
2013-10-10 12:35:22 +0000

    adapted to virtualFormExpansion.

--- handler.gui.swing/src/main/java/org/universAAL/ui/handler/gui/swing/model/FormControl/support/RepeatSubdivider.java
+++ handler.gui.swing/src/main/java/org/universAAL/ui/handler/gui/swing/model/FormControl/support/RepeatSubdivider.java
@@ -16,17 +16,12 @@
 
 package org.universAAL.ui.handler.gui.swing.model.FormControl.support;
 
-import java.util.ArrayList;
-import java.util.Enumeration;
-import java.util.Iterator;
 import java.util.List;
 
-import org.universAAL.middleware.rdf.Resource;
 import org.universAAL.middleware.ui.rdf.Form;
 import org.universAAL.middleware.ui.rdf.FormControl;
 import org.universAAL.middleware.ui.rdf.Group;
 import org.universAAL.middleware.ui.rdf.Repeat;
-import org.universAAL.middleware.ui.rdf.SubdialogTrigger;
 
 /**
  * This support class is used to generate a list of virtual forms (one per row) so each element of 
@@ -76,86 +71,6 @@ public class RepeatSubdivider {
 	 * @return
 	 */
 	public List generateSubForms() {
-		ArrayList formList = new ArrayList();
-		Object repeatData = getValue(repeat.getReferencedPPath().getThePath(),
-				repeat.getFormObject().getData());
-		List repeatList = null;
-		if (repeatData instanceof Resource) {
-			repeatList = ((Resource) repeatData).asList();
-		}
-		if (repeatData instanceof List) {
-			repeatList = (List) repeatData;
-		}
-		if (repeatData == null) {
-			// it is not initialised
-			repeatList = new ArrayList();
-		}
-		
-		if (repeatList == null) {
-			throw new IllegalArgumentException(
-					"Referenced Path for Repeat is not a list");
-		}
-			
-		int index = 0;
-		for (Iterator i = repeatList.iterator(); i.hasNext();) {
-			Resource res = (Resource) i.next();
-			Form subForm = Form.newDialog("", res);
-			for (int j = 0; j < elems.length; j++) {
-				if (elems[j] != null) {
-					FormControl nFC = (FormControl) softCopy(elems[j]);
-					nFC.changeProperty(FormControl.PROP_PARENT_CONTROL,
-							subForm.getIOControls());
-					addChild((Group) subForm.getIOControls(), nFC);
-					if (elems[j] instanceof SubdialogTrigger) {
-						nFC.changeProperty(
-								SubdialogTrigger.PROP_SUBMISSION_ID,
-								nFC.getProperty(SubdialogTrigger.PROP_REPEATABLE_ID_PREFIX)
-										+ Integer.toString(index));
-					}
-				}
-			}
-			formList.add(subForm);
-			index++;
-		}
-
-		return formList;
-	}
-
-	private static Object getValue(String[] pp, Resource pr) {
-		if (pp == null || pp.length == 0 || pr == null)
-			return null;
-
-		Object o = pr.getProperty(pp[0]);
-		for (int i = 1; o != null && i < pp.length; i++) {
-			if (!(o instanceof Resource))
-				return null;
-			pr = (Resource) o;
-			o = pr.getProperty(pp[i]);
-		}
-		return o;
-	}
-
-	private Object softCopy(Resource res) {
-		Resource newRes = Resource.getResource(res.getType(),
-				Resource.generateAnonURI());
-		Enumeration props = res.getPropertyURIs();
-		// String[] types = res.getTypes();
-		// for (int i = 0; i < types.length; i++) {
-		// newRes.addType(types[i], false);
-		// }
-		while (props.hasMoreElements()) {
-			String prop = (String) props.nextElement();
-			newRes.setProperty(prop, res.getProperty(prop));
-		}
-		return newRes;
-	}
-
-	private static void addChild(Group parent, FormControl child) {
-		List children = (List) parent.getProperty(Group.PROP_CHILDREN);
-		if (children == null) {
-			children = new ArrayList();
-			parent.setProperty(Group.PROP_CHILDREN, children);
-		}
-		children.add(child);
+		return repeat.virtualFormExpansion();
 	}
 }
