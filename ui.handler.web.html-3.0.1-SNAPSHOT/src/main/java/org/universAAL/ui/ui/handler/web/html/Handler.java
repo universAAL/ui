3c0ae6f7e33b56dbc85a72425d00d347ca003040
Alejandro Medrano <amedrano@lst.tfo.upm.es>
2014-06-05 09:26:07 +0000

    Automatic tag of ui.handler.web.html version: 3.0.1-SNAPSHOT

---
+++ ui.handler.web.html-3.0.1-SNAPSHOT/src/main/java/org/universAAL/ui/ui/handler/web/html/Handler.java
@@ -0,0 +1,164 @@
+/*******************************************************************************
+ * Copyright 2011 Universidad Politécnica de Madrid - Life Supporting Technologies
+ * Copyright 2013 Fraunhofer-Gesellschaft - Institute for Computer Graphics Research
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package org.universAAL.ui.ui.handler.web.html;
+
+import org.universAAL.middleware.container.ModuleContext;
+import org.universAAL.middleware.container.utils.LogUtils;
+import org.universAAL.middleware.owl.MergedRestriction;
+import org.universAAL.middleware.owl.supply.AbsLocation;
+import org.universAAL.middleware.rdf.Resource;
+import org.universAAL.middleware.ui.UIHandler;
+import org.universAAL.middleware.ui.UIHandlerProfile;
+import org.universAAL.middleware.ui.UIRequest;
+import org.universAAL.middleware.ui.UIResponse;
+import org.universAAL.middleware.ui.owl.Modality;
+import org.universAAL.middleware.ui.rdf.Submit;
+import org.universAAL.ontology.profile.User;
+import org.universAAL.ui.ui.handler.web.html.fm.FormManager;
+/**
+ * Main uAAL interfacing class.
+ * The {@link Handler} is responsible of interfacing with universAAL, it will handle
+ *  the UICalls by delegating to the {@link HTMLUserGenerator}, and it will also provide a method
+ *  to submit the user input.
+ *  
+ *  @see UIHandler
+ *  @see HTMLUserGenerator
+ *  
+ * 	@author amedrano
+ *
+ */
+public final class Handler extends UIHandler {
+
+	/**
+	 * internal constructor.
+	 * @param context the {@link ModuleContext} to be able to implement an {@link UIHandler}
+	 * @param initialSubscription The Subscription parameters that dictate to which {@link UIRequest}s the handler responds to
+	 * see UIHandler
+	 */
+	protected Handler(ModuleContext context,
+			UIHandlerProfile initialSubscription) {
+		super(context, initialSubscription);
+	}
+
+	/**
+	 * reference to the associated {@link HTMLUserGenerator}.
+	 */
+	private HTMLUserGenerator render;
+	
+	/**
+     * Current user that is inputting information.
+     */
+    private User currentUser = null;
+	
+	/**
+	 * constructor for Handler.
+	 * @param renderer
+	 * 	the {@link HTMLUserGenerator} to associate with
+	 * @param usr the user for this handler.
+	 */
+	Handler (HTMLUserGenerator renderer, User usr){
+		super(renderer.getModuleContext(),
+				getPermanentSubscriptions(
+						usr,
+						renderer.getUserLocation()));
+		render = renderer;
+		currentUser = usr;
+	}
+
+
+    /** {@ inheritDoc}	 */
+	public void adaptationParametersChanged(String dialogID,
+			String changedProp, Object newVal) {
+		String text = "Adaptation Parameters Changed\n";
+		text += dialogID + " Prop: " + changedProp + "\n";
+		text += "prop of type: " + newVal.getClass().getName();
+		
+		LogUtils.logDebug(super.owner, getClass(),
+				"adaptationParametersChanged", new String[] {text}, null);
+		render.getFormManagement().adaptationParametersChanged(dialogID, changedProp, newVal);
+	}
+
+	/** {@ inheritDoc}	 */
+	public void communicationChannelBroken() {
+		
+	}
+
+	/** {@ inheritDoc}*/
+	public Resource cutDialog(String dialogID) {
+		return render.getFormManagement().cutDialog(dialogID);
+	}
+
+	/**
+     * Handle {@link UIRequest}.
+     * this callback method will trigger the rendering process for the {@link UIRequest}.
+     * It will also extract important information, so the handler can adapt to
+     * the user's circumstances.
+     */
+    public void handleUICall(UIRequest req) {
+    	FormManager fm = render.getFormManagement();
+    	fm.addDialog(req);
+    }
+
+    /**
+     * Get the current user that is inputing information.
+     * @return
+     *     the current user.
+     */
+    public User getCurrentUser() {
+        return currentUser;
+    }
+    
+    /**
+     * Shortcut to send {@link UIResponse} related to Submit Buttons.
+     * @param submit
+     *       the {@link Submit} button model pressed.
+     */
+    public void submit(Submit submit) {
+        dialogFinished(
+                new UIResponse(
+                        currentUser,
+                        render.getUserLocation(),
+                        submit));
+    }
+
+    
+    /**
+     * States the pattern of interesting UIRequests, for the handler.
+     *
+     * used to create the pattern needed for {@link Handler}.
+     *
+     * @return a pattern used to subscribe to the output bus.
+     * @param usr set demo mode or not.
+     * 
+     */
+    private static UIHandlerProfile getPermanentSubscriptions(final User usr, AbsLocation location) {
+        /*
+         * Handler is interested in all UIRequests with following UIRequestPattern
+         * restrictions:
+         */
+    	UIHandlerProfile oep = new UIHandlerProfile();
+    	oep.addRestriction(MergedRestriction.getFixedValueRestriction(
+    			UIRequest.PROP_ADDRESSED_USER, usr)); 
+    	oep.addRestriction(MergedRestriction.getFixedValueRestriction(
+    			UIRequest.PROP_PRESENTATION_MODALITY, Modality.web)); 
+    	oep.addRestriction(MergedRestriction.getFixedValueRestriction(
+    			UIRequest.PROP_PRESENTATION_LOCATION,
+    			location));
+    	oep.setSupportedInputModalities(new Modality[]{Modality.web});
+    	return oep;
+    }
+}
---
+++ ui.handler.web.html-3.0.1-SNAPSHOT/src/main/java/org/universAAL/ui/ui/handler/web/html/model/InputFieldModel.java
@@ -0,0 +1,180 @@
+/*******************************************************************************
+ * Copyright 2013 Universidad Politécnica de Madrid - Life Supporting Technologies
+ * Copyright 2013 Fraunhofer-Gesellschaft - Institute for Computer Graphics Research
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+
+package org.universAAL.ui.ui.handler.web.html.model;
+
+
+import java.util.Properties;
+
+import javax.xml.datatype.DatatypeConfigurationException;
+import javax.xml.datatype.DatatypeConstants;
+import javax.xml.datatype.DatatypeFactory;
+import javax.xml.datatype.XMLGregorianCalendar;
+import javax.xml.namespace.QName;
+
+import org.universAAL.middleware.container.utils.LogUtils;
+import org.universAAL.middleware.ui.rdf.InputField;
+import org.universAAL.ui.ui.handler.web.html.HTMLUserGenerator;
+
+/**
+ * @author amedrano
+ * 
+ */
+public class InputFieldModel extends InputModel {
+
+	/**
+	 * @param fe
+	 * @param render
+	 */
+	public InputFieldModel(InputField fe, HTMLUserGenerator render) {
+		super(fe, render);
+	}
+
+	/** {@ inheritDoc} */
+	public boolean updateInput(String[] strings) {
+		boolean res = false;
+		try {
+			InputField i = (InputField) fe;
+			if ( strings != null && strings.length > 0) {
+				Object val = i.getValue();
+				if (i.isOfBooleanType()){
+					res = i.storeUserInput(Boolean.valueOf(strings[0]));
+				}
+				if (isOfType(Integer.class)) {
+					res = i.storeUserInput(Integer.decode(strings[0]));
+				}
+				if (isOfType(Long.class)){
+					res = i.storeUserInput(Long.decode(strings[0]));				
+				}
+				if (isOfType(Float.class)){
+					res = i.storeUserInput(new Float(Float.parseFloat(strings[0])));
+					
+				}
+				if (isOfType(Double.class)){
+					res = i.storeUserInput(new Double(Double.parseDouble(strings[0])));
+				}
+				if (isOfType(XMLGregorianCalendar.class)){
+					try {
+						val = DatatypeFactory.newInstance().newXMLGregorianCalendar(strings[0]);
+						res = i.storeUserInput(val);
+					} catch (DatatypeConfigurationException e1) {
+						LogUtils.logError(getRenderer().getModuleContext(),
+								getClass(), 
+								"updateInput", "unable to convert \"" 
+								+ strings[0]+ "\" to XMLGregorianCalendar");
+					}
+				}
+				if(val == null || !res){
+					res=i.storeUserInput(strings[0]);
+				}
+			}
+		} catch (Exception e) {
+			LogUtils.logInfo(getRenderer().getModuleContext(), 
+					getClass(), "updateImput", new String[]{"unable to update input "}, e);
+		}
+		return res;
+	}
+
+	/**
+	 * the representation for InputField can either be
+	 * <ul>
+	 * <li>a checkbox if the {@link InputField#getValue()} is a boolean type
+	 * <li>a text if the {@link InputField#getValue()} is a String and not
+	 * secret
+	 * <li>a password if the {@link InputField#getValue()} is String and it is
+	 * secret
+	 * <li>a datetime-local if the {@link InputField#getValue()} is a
+	 * XMLGregorianCalendar
+	 * <li>a ?? if the {@link InputField#getValue()} is a Duration
+	 * <li>a number if the {@link InputField#getValue()} is a Integer
+	 * <li>a number if the {@link InputField#getValue()} is a Long
+	 * <li>a number if the {@link InputField#getValue()} is a Float
+	 * <li>a number if the {@link InputField#getValue()} is a Double
+	 * <li>a ?? if the {@link InputField#getValue()} is a Locale
+	 * </ul>
+	 * 
+	 * @return {@inheritDoc}
+	 */
+	public StringBuffer generateInputHTML() {
+		InputField i = (InputField) fe;
+		Object val = i.getValue();
+		setInputTypeProperties(fcProps, val);
+		if (i.isSecret()){
+			fcProps.put("type", "password");
+		} 
+		StringBuffer defaultInput = new StringBuffer();
+		if (i.isOfBooleanType()){
+			Properties defInProp = new Properties();
+			defInProp.put("type", "hidden");
+			defInProp.put("name", fcProps.get("name"));
+			defInProp.put("value", "false");
+			defaultInput.append(singleTag("input",defInProp ));
+		}
+		return singleTag("input", fcProps).append(defaultInput);
+	}
+
+	/**
+	 * Add properties to input properties. According to type of val
+	 * the type and value of the input will be generated.
+	 * @param prop the properties to edit
+	 * @param val the value to work with.
+	 */
+	public static void setInputTypeProperties(Properties prop, Object val) {
+		// Default.
+		prop.put("type", "text");
+		if (val != null
+				&& !val.toString().isEmpty()){
+			prop.put("value", val.toString());
+		}
+
+		if (val instanceof Boolean) {
+			prop.put("type", "checkbox");
+			prop.put("value", "true");
+			if (((Boolean)val).booleanValue()){
+				prop.put("checked", "");
+			}
+		}
+		if (val instanceof XMLGregorianCalendar){
+			QName type = ((XMLGregorianCalendar) val).getXMLSchemaType();
+
+			if (type.equals(DatatypeConstants.DATETIME)){
+				prop.put("type", "datetime-local");
+				prop.put("value", ((XMLGregorianCalendar) val).toString());
+			}
+			if (type.equals(DatatypeConstants.TIME)){
+				prop.put("type", "time");
+				prop.put("value", ((XMLGregorianCalendar) val).toString());
+			}
+			if (type.equals(DatatypeConstants.DATE)){
+				prop.put("type", "date");
+				prop.put("value", ((XMLGregorianCalendar) val).toString());
+			}
+		}
+		if (val instanceof Integer
+				|| val instanceof Long){
+			prop.put("type", "number");
+			prop.put("step", "1");
+			prop.put("pattern", "[-+]?[0-9]*");
+		}
+		if (val instanceof Float
+				|| val instanceof Double){
+			prop.put("type", "number");
+			prop.put("step", "any");
+			prop.put("pattern", "[-+]?[0-9]*\\.?[0-9]+");
+		}
+	}
+}
