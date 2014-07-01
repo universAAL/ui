c60223c535bc66f4d1cf1aec9db0a4fd5ddc2c24
Alejandro Medrano <amedrano@lst.tfo.upm.es>
2014-07-01 09:40:38 +0000

    optimized rendering of rounded buttons, added floating effect.

--- ui.handler.gui.swing/ExtraLookAndFeelPackages/bluesteelLAF/src/main/java/org/universAAL/ui/gui/swing/bluesteelLAF/support/RoundedGradientButton.java
+++ ui.handler.gui.swing/ExtraLookAndFeelPackages/bluesteelLAF/src/main/java/org/universAAL/ui/gui/swing/bluesteelLAF/support/RoundedGradientButton.java
@@ -17,16 +17,19 @@ package org.universAAL.ui.gui.swing.bluesteelLAF.support;
 
 import java.awt.BasicStroke;
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.GradientPaint;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Paint;
 import java.awt.Rectangle;
 import java.awt.RenderingHints;
+import java.awt.Shape;
 import java.awt.event.ActionListener;
 import java.awt.geom.RoundRectangle2D;
 
 import javax.swing.AbstractButton;
+import javax.swing.ButtonModel;
 import javax.swing.Icon;
 import javax.swing.JButton;
 import javax.swing.JComponent;
@@ -65,6 +68,9 @@ public class RoundedGradientButton extends JButton {
 	    setBorderPainted(false);
 	    setOpaque(true);
 	    setUI(new UIRoundedGradientButton());
+	    setContentAreaFilled(false);
+	    setFocusPainted(false);
+	    getShape();
 	}
 	
 	public RoundedGradientButton(AbstractButton button, Color light, Color dark){
@@ -89,70 +95,90 @@ public class RoundedGradientButton extends JButton {
 	}
 	
 	
+
+	  protected Shape shape, base;
+
+	  
+	  protected Shape getShape(){
+		  if(!getBounds().equals(base)) {
+		      Dimension s = getSize();
+		      base = getBounds();
+		      shape = new RoundRectangle2D.Float(0,0,s.width-1,s.height-1,17,17);
+		    }
+		  return shape;
+	  }
+	  
+	  @Override 
+	  public boolean contains(int x, int y) {
+	    return getShape().contains(x, y);
+	  }
+	  
 	class UIRoundedGradientButton extends MetalButtonUI{
 
-		/* (non-Javadoc)
-		 * @see javax.swing.plaf.metal.MetalButtonUI#update(java.awt.Graphics, javax.swing.JComponent)
-		 */
+		/** {@inheritDoc} */
 		@Override
 		public void update(Graphics g, JComponent c) {
 			Graphics2D g2 = (Graphics2D) g;
 	        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,RenderingHints.VALUE_ANTIALIAS_ON);
 
-	        //ButtonModel m = getModel();
+	        ButtonModel m = getModel();
 
 	        Paint oldPaint = g2.getPaint();
 	      
-	        RoundRectangle2D.Float r2d = new RoundRectangle2D.Float(0,0,getWidth(),getHeight()-1,17,17);
-	            g2.clip(r2d);
-	            g2.setPaint(new GradientPaint(0.0f, 0.0f, light,
-	                    0.0f, getHeight(), dark));
-	            g2.fillRect(0,0,getWidth(),getHeight());
+	        g2.clip(getShape());
+	        g2.setPaint(new GradientPaint(0.0f, 0.0f, light,
+	        		0.0f, getHeight(), dark));
+	        g2.fillRect(0,0,getWidth(),getHeight());
 
-	            g2.setStroke(new BasicStroke(4f));
-	            g2.setPaint(new GradientPaint(0.0f, 0.0f, light,
-	                    0.0f, getHeight(), dark));
-	            g2.drawRoundRect(0, 0, getWidth()-1 , getHeight() -1, 15, 15);
+	        g2.setStroke(new BasicStroke(4f));
+	        g2.setPaint(new GradientPaint(0.0f, 0.0f, light,
+	        		0.0f, getHeight(), dark));
 
 	        g2.setPaint(oldPaint);
-	        getRaisedBorder().paintBorder(c, g2, 0, 0, getWidth(), getHeight());
-	        paint(g2,c);
+	        
+	        if (!m.isRollover()) {
+				getRaisedBorder().paintBorder(c, g2, 0, 0, getWidth(),
+						getHeight());
+			} else {
+				Border b = new SoftBevelBorder(SoftBevelBorder.RAISED, bDark , bLight.brighter());
+				b.paintBorder(c, g2, 0, 0, getWidth(),
+						getHeight());
+			}
+				paint(g2, c);
+			g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+	        		RenderingHints.VALUE_ANTIALIAS_OFF);
 		}
 
-		/* (non-Javadoc)
-		 * @see javax.swing.plaf.metal.MetalButtonUI#paintButtonPressed(java.awt.Graphics, javax.swing.AbstractButton)
-		 */
+		/** {@inheritDoc} */
 		@Override
 		protected void paintButtonPressed(Graphics g, AbstractButton b) {
 			Graphics2D g2 = (Graphics2D) g;
 	        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,RenderingHints.VALUE_ANTIALIAS_ON);
 
-	        //ButtonModel m = getModel();
-
 	        Paint oldPaint = g2.getPaint();
 	      
-	        RoundRectangle2D.Float r2d = new RoundRectangle2D.Float(0,0,getWidth(),getHeight()-1,17,17);
-	            g2.clip(r2d);
-	            g2.setPaint(new GradientPaint(0.0f, 0.0f, light.brighter(),
-	                    0.0f, getHeight(), dark.brighter()));
-	            g2.fillRect(0,0,getWidth(),getHeight());
+	        g2.clip(getShape());
+	        g2.setPaint(new GradientPaint(0.0f, 0.0f, light.brighter(),
+	        		0.0f, getHeight(), dark.brighter()));
+	        g2.fillRect(0,0,getWidth(),getHeight());
 
-	            g2.setStroke(new BasicStroke(4f));
-	            g2.setPaint(new GradientPaint(0.0f, 0.0f, light.brighter(),
-	                    0.0f, getHeight(), dark.brighter()));
-	            g2.drawRoundRect(0, 0, getWidth()-1 , getHeight() -1, 15, 15);
+	        g2.setStroke(new BasicStroke(4f));
+	        g2.setPaint(new GradientPaint(0.0f, 0.0f, light.brighter(),
+	        		0.0f, getHeight(), dark.brighter()));
 
 	        g2.setPaint(oldPaint);
 	        getLoweredBorder().paintBorder(b, g2, 0, 0, getWidth(), getHeight());
+	        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+	        		RenderingHints.VALUE_ANTIALIAS_OFF);
 		}
 
 		/** {@inheritDoc} */
 		@Override
 		protected void paintFocus(Graphics g, AbstractButton b,
 				Rectangle viewRect, Rectangle textRect, Rectangle iconRect) {
+			
 		}
 		
-		//TODO: paint hover effect.
 	}
 
 }
