217d4ee3d3bf83c260cc2d64ba657ab77edd3ccd
Alejandro Medrano <amedrano@lst.tfo.upm.es>
2014-10-15 14:54:09 +0000

    Automatic tag of ui.dm version: 3.2.0

---
+++ ui.dm/src/main/java/org/universAAL/ui/dm/userInteraction/mainMenu/profilable/RDFMainMenu.java
@@ -0,0 +1,183 @@
+/*	
+	Copyright 2007-2014 Fraunhofer IGD, http://www.igd.fraunhofer.de
+	Fraunhofer-Gesellschaft - Institut für Graphische Datenverarbeitung
+	
+	See the NOTICE file distributed with this work for additional 
+	information regarding copyright ownership
+	
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+	
+	  http://www.apache.org/licenses/LICENSE-2.0
+	
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+ */
+package org.universAAL.ui.dm.userInteraction.mainMenu.profilable;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.nio.charset.Charset;
+
+import org.universAAL.middleware.container.ModuleContext;
+import org.universAAL.middleware.container.utils.LogUtils;
+import org.universAAL.middleware.rdf.Resource;
+import org.universAAL.middleware.serialization.MessageContentSerializer;
+import org.universAAL.ontology.profile.ui.mainmenu.MenuEntry;
+import org.universAAL.ontology.profile.ui.mainmenu.MenuProfile;
+import org.universAAL.ui.dm.userInteraction.mainMenu.file.MainMenu;
+
+public class RDFMainMenu extends MainMenu {
+
+    private static final String UTF_8 = "utf-8";
+    private ModuleContext context;
+
+    public RDFMainMenu(ModuleContext ctxt, InputStream in) {
+	this(ctxt);
+	constructMenu(in);
+    }
+    
+    public RDFMainMenu(ModuleContext ctxt) {
+    	context=ctxt;
+    }
+
+    protected void constructMenu(InputStream in) {
+
+	RDFMenuNode rdfroot = new RDFMenuNode(-1);
+	root = rdfroot;
+	selection = root;
+
+	// Read the configuration file of the menu for this user which
+	// contains a list of menu entries.
+	Resource r = null;
+	try {
+	    r = readMenu(in);
+	} catch (Exception e) {
+	    r = null;
+	}
+	if (r == null) {
+	    LogUtils.logError(context, RDFMainMenu.class, "constructMenu",
+		    new Object[] { "resource is null" }, null);
+	    return;
+	}
+	if (!(r instanceof MenuProfile)) {
+	    LogUtils.logError(context, RDFMainMenu.class, "constructMenu",
+		    new Object[] { "resource is not a UIMainMenuProfile" },
+		    null);
+	    return;
+	}
+
+	MenuProfile mm = (MenuProfile) r;
+	MenuEntry[] entries = mm.getMenuEntries();
+
+	for (int i = 0; i < entries.length; i++)
+	    rdfroot.add(entries[i]);
+    }
+
+    protected Resource readMenu(InputStream in) {
+	// read the file
+	BufferedReader br = new BufferedReader(new InputStreamReader(in,
+		Charset.forName(UTF_8)));
+	StringBuilder sb = new StringBuilder();
+	try {
+	    String line = br.readLine();
+	    while (line != null) {
+		sb.append(line);
+		sb.append('\n');
+		line = br.readLine();
+	    }
+	} catch (IOException e) {
+	    LogUtils.logError(context, RDFMainMenu.class, "readMenu",
+		    new Object[] { "cannot read the file" }, e);
+	    return null;
+	}
+	String serialized = sb.toString();
+	if (serialized.length() < 5) {
+	    // just a small value to indicate that the file is empty
+	    LogUtils.logError(context, RDFMainMenu.class, "readMenu",
+			    new Object[] { "file is considered empty." }, null);
+	    return null;
+	}
+
+	// deserialize
+	MessageContentSerializer contentSerializer = (MessageContentSerializer) context
+		.getContainer()
+		.fetchSharedObject(
+			context,
+			new Object[] { MessageContentSerializer.class.getName() });
+	if (contentSerializer == null) {
+	    LogUtils.logError(context, RDFMainMenu.class, "readMenu",
+		    new Object[] { "no serializer found" }, null);
+	    return null;
+	}
+
+	return (Resource) contentSerializer.deserialize(serialized);
+    }
+
+    public Resource readMenu(File file) throws FileNotFoundException {
+	try {
+	    InputStream in = new FileInputStream(file);
+	    Resource r = readMenu(in);
+	    in.close();
+	    return r;
+	} catch (FileNotFoundException e){
+	  throw e;  
+	} catch (Exception e) {
+	     LogUtils
+	     .logError(
+	     context,
+	     RDFMainMenu.class,
+	     "readMenu",
+	     new Object[] { "Unexpected error while reading file: " + file.getName() },
+	     e);
+	     throw new RuntimeException(e.getMessage());
+	}
+//	return null;
+    }
+
+    public void saveMenu(File file, MenuProfile mp) {
+	try {
+	    OutputStream out = new FileOutputStream(file);
+	    saveMenu(out, mp);
+	    out.close();
+	} catch (Exception e) {
+	    LogUtils.logError(context, RDFMainMenu.class, "saveMenu",
+		    new Object[] { "" }, e);
+	    throw new RuntimeException(e.getMessage());
+	}
+    }
+
+    public void saveMenu(OutputStream out, MenuProfile mp) {
+	// serialize
+	MessageContentSerializer contentSerializer = (MessageContentSerializer) context
+		.getContainer()
+		.fetchSharedObject(
+			context,
+			new Object[] { MessageContentSerializer.class.getName() });
+	if (contentSerializer == null) {
+	    LogUtils.logError(context, RDFMainMenu.class, "saveMenu",
+		    new Object[] { "no serializer found" }, null);
+	    throw new RuntimeException("no serializer found");
+	}
+	String serialized = contentSerializer.serialize(mp);
+	// write
+	try {
+	    out.write(serialized.getBytes(Charset.forName(UTF_8)));
+	} catch (IOException e) {
+	    LogUtils.logError(context, RDFMainMenu.class, "saveMenu",
+		    new Object[] { "" }, e);
+	    throw new RuntimeException("io exception");
+	}
+    }
+}
---
+++ ui.dm/src/test/java/org/universAAL/ui/dm/tests/UIRequestPoolTest.java
@@ -0,0 +1,158 @@
+/*******************************************************************************
+ * Copyright 2011 Universidad Politécnica de Madrid
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package org.universAAL.ui.dm.tests;
+
+import java.util.Iterator;
+import java.util.Locale;
+import java.util.Random;
+
+import junit.framework.TestCase;
+
+import org.universAAL.container.JUnit.JUnitModuleContext;
+import org.universAAL.middleware.container.ModuleContext;
+import org.universAAL.middleware.owl.DataRepOntology;
+import org.universAAL.middleware.owl.OntologyManagement;
+import org.universAAL.middleware.owl.supply.LevelRating;
+import org.universAAL.middleware.rdf.Resource;
+import org.universAAL.middleware.ui.UIRequest;
+import org.universAAL.middleware.ui.owl.PrivacyLevel;
+import org.universAAL.middleware.ui.owl.UIBusOntology;
+import org.universAAL.middleware.ui.rdf.Form;
+import org.universAAL.ui.dm.interfaces.IUIRequestPool;
+
+public abstract class UIRequestPoolTest extends TestCase {
+
+    protected static final String MY_USER = "http://universaal.org/Users#TestUser";
+    private static IUIRequestPool pool;
+
+    public abstract IUIRequestPool initialisePool();
+
+    ModuleContext mc = new JUnitModuleContext();
+
+    public void setUp() {
+	OntologyManagement.getInstance().register(mc, new DataRepOntology());
+	OntologyManagement.getInstance().register(mc, new UIBusOntology());
+    }
+
+    public void test1() {
+	System.out.println("Test1 :");
+	pool = initialisePool();
+	assertEquals(0, pool.listAllActive().size());
+	assertEquals(0, pool.listAllSuspended().size());
+	assertNull(pool.getCurrent());
+	assertFalse(pool.hasToChange());
+	printPool();
+//    }
+//
+//    public void test2() {
+	System.out.println("Test2 :");
+	UIRequest req = getNewRequest();
+	pool.add(req);
+	assertEquals(1, pool.listAllActive().size());
+	assertEquals(0, pool.listAllSuspended().size());
+	assertTrue(pool.hasToChange());
+	printPool();
+//    }
+//
+//    public void test3() {
+	// System.out.println("Test3 :");
+	// try {
+	// Thread.sleep(500);
+	// } catch (InterruptedException e) { }
+	req = getNewRequest();
+	System.out.println("NEW REQUEST ID: " + req.getDialogID());
+	pool.add(req);
+	assertEquals(2, pool.listAllActive().size());
+	assertEquals(0, pool.listAllSuspended().size());
+	printPool();
+//    }
+//
+//    public void test4() {
+
+	System.out.println("Test4 :");
+	UIRequest next = pool.getNextUIRequest();
+	assertNotNull(next);
+	assertEquals(pool.getCurrent(), next);
+	printPool();
+//    }
+//
+//    public void test5() {
+	System.out.println("Test5 :");
+	pool.suspend(pool.getCurrent().getDialogID());
+	assertNull(pool.getCurrent());
+	assertEquals(1, pool.listAllActive().size());
+	assertEquals(1, pool.listAllSuspended().size());
+	printPool();
+//    }
+//
+//    public void test6() {
+	System.out.println("Test6 :");
+	Iterator<UIRequest> i = pool.listAllSuspended().iterator();
+	String r = i.next().getDialogID();
+	pool.unsuspend(r);
+	assertEquals(2, pool.listAllActive().size());
+	assertEquals(0, pool.listAllSuspended().size());
+	printPool();
+//    }
+//
+//    public void test7() {
+	System.out.println("Test7 :");
+	req = pool.getNextUIRequest();
+	pool.suspend(req.getDialogID());
+	assertEquals(req, pool.get(req.getDialogID()));
+	printPool();
+//    }
+//
+//    public void test8() {
+	Iterator<UIRequest> it = pool.listAllSuspended().iterator();
+	String rURI = it.next().getDialogID();
+	pool.close(rURI);
+	assertEquals(1, pool.listAllActive().size());
+	assertEquals(0, pool.listAllSuspended().size());
+	it = pool.listAllActive().iterator();
+	rURI = it.next().getDialogID();
+	pool.close(rURI);
+	assertEquals(0, pool.listAllActive().size());
+	assertEquals(0, pool.listAllSuspended().size());
+	assertNull(pool.getCurrent());
+    }
+
+    private UIRequest getNewRequest() {
+	Form dialogForm = Form.newMessage("test message", "testing messages"
+		+ new Random().nextLong());
+	UIRequest req = new UIRequest(new Resource(MY_USER), dialogForm,
+		LevelRating.none, Locale.ENGLISH, PrivacyLevel.insensible);
+	return req;
+    }
+
+    private void printPool() {
+	System.out.println("ActiveSet :");
+	for (UIRequest r : pool.listAllActive()) {
+	    System.out.println(r.getDialogID());
+	}
+	System.out.println("SuspendendSet :");
+	for (UIRequest r : pool.listAllSuspended()) {
+	    System.out.println(r.getDialogID());
+	}
+	System.out.println("Current :");
+	if (pool.getCurrent() != null) {
+	    System.out.println(pool.getCurrent().getDialogID());
+	} else {
+	    System.out.println("null");
+	}
+    }
+
+}
